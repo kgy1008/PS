57c33a8e7da1da4825610c97cd6cf723a50c4895
kgy1008 <gayeon1008@g.skku.edu>
2024-08-07 16:09:28 +0900

    [level 1] Title: 모의고사, Time: 5.49 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,37 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        ArrayList<Integer> result = new ArrayList<>();
-        int[] arr1 = {1,2,3,4,5};
-        int[] arr2 = {2,1,2,3,2,4,2,5};
-        int[] arr3 = {3,3,1,1,2,2,4,4,5,5};
+        int[][] arr = {
+            {1,2,3,4,5},
+            {2,1,2,3,2,4,2,5},
+            {3,3,1,1,2,2,4,4,5,5}
+        };
         
-        int person1 = 0;
-        int person2 = 0;
-        int person3 = 0;
+        int[] score = new int[3];
         
         for(int i = 0; i < answers.length; i++) {
-            if (arr1[i % arr1.length] == answers[i]) {
-                person1 += 1;
-            }
-            if (arr2[i % arr2.length] == answers[i]) {
-                person2 += 1;
-            }
-            if (arr3[i % arr3.length] == answers[i]) {
-                person3 += 1;
+            for (int j = 0; j < 3; j ++) {
+                if (arr[j][i % arr[j].length] == answers[i]) {
+                    score[j]++;
+                }
             }
         }
         
-        int max = Math.max(person1, Math.max(person2, person3));
-        
-        if (person1 == max) {
-            result.add(1);
-        }
-        if (person2 == max) {
-            result.add(2);
-        }
-        if (person3 == max) {
-            result.add(3);
+        int maxScore = Arrays.stream(score).max().getAsInt();
+        ArrayList<Integer> result = new ArrayList<>();
+    
+        for (int i=0; i<3; i++) {
+            if (maxScore == score[i]) {
+                result.add(i+1);
+            }
         }
         
         return result.stream()
