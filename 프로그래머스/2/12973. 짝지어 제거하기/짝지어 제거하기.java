64499de6cd33b2ae3e6df40047918f1fcbf60460
kgy1008 <gayeon1008@g.skku.edu>
2024-08-17 23:38:22 +0900

    [level 2] Title: 짝지어 제거하기, Time: 50.10 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -2,28 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
-        int answer = 0;
-        
         ArrayDeque<Character> stack = new ArrayDeque<>();
         
         for (int i = 0; i < s.length(); i++) {
             char temp = s.charAt(i);
-            if (i == 0 || stack.isEmpty()) {
+            if (!stack.isEmpty() && stack.peek() == temp) {
+                stack.pop();
+            } else {
                 stack.push(temp);
-            }
-            else {
-                if (stack.peek() == temp) {
-                    stack.pop();
-                }
-                else {
-                    stack.push(temp);
-                }
             }
         }
         
-        if (stack.isEmpty()) {
-            answer = 1;
-        }
-        return answer;
+        return stack.isEmpty() ? 1 : 0;
     }
 }
