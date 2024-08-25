34cad5202e800c8ce88e4e542fb4fdb009156c04
kgy1008 <gayeon1008@g.skku.edu>
2024-08-25 17:19:33 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 7628.06 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,30 +1,44 @@
 import java.util.HashMap;
 
-public class Solution {
+class Person {
+    String name;
+    Person parent;
+    int money;
 
-    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        HashMap<String, String> parent = new HashMap<>();
-        for (int i = 0; i < enroll.length; i++) {
-            parent.put(enroll[i], referral[i]);
-        }
-
-        HashMap<String, Integer> total = new HashMap<>();
-
-        for (int i = 0; i < seller.length; i++) {
-            String curName = seller[i];
-            int money = amount[i] * 100;
-            while (money > 0 && !curName.equals("-")) {
-                total.put(curName, total.getOrDefault(curName, 0) + money - (money / 10));
-                curName = parent.get(curName);
-                money /= 10;
-            }
-        }
-
-        int[] answer = new int[enroll.length];
-        for (int i = 0; i < enroll.length; i++) {
-            answer[i] = total.getOrDefault(enroll[i], 0);
-        }
-        return answer;
+    public Person(String name, Person parent, int money) {
+        this.name = name;
+        this.parent = parent;
+        this.money = money;
     }
 
+    void getReward(int i) {
+        int moneyToParent = (int) (i * 0.1);
+        this.money += i - moneyToParent;
+        if (this.parent != null)
+            this.parent.getReward(moneyToParent);
+    }
+}
+
+class Solution {
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        HashMap<String, Person> personHashMap = new HashMap<>();
+        for (String name : enroll)
+            personHashMap.put(name, new Person(name, null, 0));
+
+        for (int i = 0; i < enroll.length; i++) {
+            if (referral[i].equals("-"))
+                continue;
+            personHashMap.get(enroll[i]).parent = personHashMap.get(referral[i]);
+        }
+
+        for (int i = 0; i < seller.length; i++)
+            personHashMap.get(seller[i]).getReward(amount[i] * 100);
+
+        int[] result = new int[enroll.length];
+
+        for (int i = 0; i < result.length; i++)
+            result[i] = personHashMap.get(enroll[i]).money;
+
+        return result;
+    }
 }
