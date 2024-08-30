07c898f1313e17d4503b1c5df1074aac6829c8b5
kgy1008 <gayeon1008@g.skku.edu>
2024-08-31 01:31:55 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.12 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,20 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        HashSet<String> sentence = new HashSet<>();
         int[] answer = new int[2];
-        sentence.add(words[0]);
-        String previous = words[0];
+        HashSet<String> sentence = new HashSet<>();
+        char previous = words[0].charAt(0);
         
-        for (int i = 1; i<words.length; i++) {
-            if (sentence.contains(words[i]) || previous.charAt(previous.length()-1) != words[i].charAt(0)) {
+        for (int i = 0; i<words.length; i++) {
+            if (sentence.contains(words[i]) || previous != words[i].charAt(0)) {
                 answer[1] = i/n + 1;
                 answer[0] = i%n + 1;
                 return answer;
             }
             
             sentence.add(words[i]);
-            previous = words[i];
+            previous = words[i].charAt(words[i].length() - 1);
         }
         
         return answer;
