7844eff81bbeed7f6ad02f0acb4b2ecbe3c5a6a3
kgy1008 <gayeon1008@g.skku.edu>
2024-08-31 01:08:50 +0900

    [level 1] Title: 폰켓몬, Time: 3.86 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,19 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int choice = nums.length / 2;
+        
         HashSet<Integer> kinds = new HashSet<>();
         
         for (int n : nums) {
             kinds.add(n);
         }
         
-        int total = kinds.size();
+        int choice = nums.length / 2;
         
-        if (choice <=  total) {
-            return choice;
-        } else {
-            return total;
-        }
+        return Math.min(choice, kinds.size());
     }
 }
