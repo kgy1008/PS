a75b4ee61160af5ab447460bf62320f5ab03c312
kgy1008 <gayeon1008@g.skku.edu>
2024-09-03 19:00:03 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 80.63 ms, Memory: 145 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,38 +1,35 @@
 import java.util.*;
 
 class Solution {
-    
+
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
         HashMap<String, String> user = new HashMap<>();
         HashMap<String, Integer> sell = new HashMap<>();
         int[] answer = new int[enroll.length];
-        
-        HashMap<String, Integer> indexMap = new HashMap<>();
-        
+
         for (int i=0; i<enroll.length; i++) {
             user.put(enroll[i], referral[i]);
-            indexMap.put(enroll[i], i);
         }
-        
+
         for (int i=0; i<seller.length; i++) {
             String s = seller[i];
             int money = amount[i] * 100;
             distributeProfit(s, money, user, sell);
         }
-        
+
         for (int i=0; i<enroll.length; i++) {
             answer[i] = sell.getOrDefault(enroll[i], 0);
         }
-        
+
         return answer;
     }
-    
+
     private void distributeProfit(String s, int money, HashMap<String, String> user, HashMap<String, Integer> sell) {
-        if (s.equals("-") || money < 1) return;
-        
+        if (s.equals("-") || money == 0) return;
+
         int profitToKeep = money - (money / 10);
         sell.put(s, sell.getOrDefault(s, 0) + profitToKeep);
-        
+
         distributeProfit(user.get(s), money / 10, user, sell);
     }
 }
