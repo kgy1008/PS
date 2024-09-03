328a725d419aa4a1cd736a14befdaf6269cb7f24
kgy1008 <gayeon1008@g.skku.edu>
2024-09-03 17:02:56 +0900

    [level 2] Title: 오픈채팅방, Time: 398.51 ms, Memory: 334 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -2,26 +2,29 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] record) {
-        HashMap<String, String> users = new HashMap<>();
-        ArrayList<String> result = new ArrayList<>();
+        HashMap<String, String> nickname = new HashMap<>();
+        List<String> ans = new ArrayList<>();
         
         for (String r : record) {
-            String[] information = r.split(" ");
-            if (information.length == 3) {
-                users.put(information[1], information[2]);
+            String[] command = r.split(" ");
+            if (!command[0].equals("Leave")) {
+                nickname.put(command[1], command[2]);
             }
         }
         
         for (String r : record) {
-            String[] cmd = r.split(" ");
-           if (cmd[0].equals("Enter")) {
-                result.add(String.format("%s님이 들어왔습니다.", users.get(cmd[1])));
+            String[] command = r.split(" ");
+            String user = nickname.get(command[1]);
+            
+            if (command[0].equals("Leave")) {
+                ans.add(String.format("%s님이 나갔습니다.", user));
             }
-            else if (cmd[0].equals("Leave")) {
-                result.add(String.format("%s님이 나갔습니다.", users.get(cmd[1])));
+            else if (command[0].equals("Enter")) {
+                ans.add(String.format("%s님이 들어왔습니다.", user));
             }
         }
         
-        return result.toArray(new String[0]);
+        return ans.toArray(new String[0]);
+        
     }
 }
