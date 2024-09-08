cc66ce861ebbb9c232aad37e0062da1a2f9379ff
kgy1008 <gayeon1008@g.skku.edu>
2024-09-09 01:03:19 +0900

    [level 2] Title: 괄호 회전하기, Time: 49.89 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -2,35 +2,41 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
-        HashMap<Character, Character> map = new HashMap<>(); 
-        map.put(')', '('); 
-        map.put('}', '{');
-        map.put(']', '[');
-        
-        int n = s.length();
-        s += s; 
         int answer = 0;
+        int len = s.length();
 
-        A: for (int i = 0; i < n; i++) { 
-            ArrayDeque<Character> stack = new ArrayDeque<>();
-            
-            for (int j = i; j < i + n; j++) { 
-                char c = s.charAt(j);
-                
-                if (!map.containsKey(c)) {
-                    stack.push(c);
-                } else {
-                    if (stack.isEmpty() || !stack.pop().equals(map.get(c))) {
-                        continue A;
-                    }
-                }
-            }
-                        
-            if (stack.isEmpty()) {
+        for (int i = 0; i < len; i++) {
+            String rotated = s.substring(i) + s.substring(0, i);
+            if (test(rotated.toCharArray())) {
                 answer++;
             }
         }
         
         return answer;
+    }
+    
+    private boolean test(char[] a) {
+        Deque<Character> stack = new ArrayDeque<>(); 
+
+        for (char bracket : a) {
+            if (bracket == '(' || bracket == '[' || bracket == '{') {
+                stack.push(bracket);
+            }
+        
+            else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                
+                char test = stack.peek();
+                
+                if (test == '(' && bracket == ')' ||
+                   test == '{' && bracket == '}' ||
+                   test == '[' && bracket == ']') {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty();
     }
 }
