0e8e5a99db6ae3c17fbd7aac92cd59f20b23b9ff
kgy1008 <gayeon1008@g.skku.edu>
2024-09-09 00:45:41 +0900

    [level 2] Title: 괄호 회전하기, Time: 71.21 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -2,33 +2,32 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
-        HashMap<Character, Character> map = new HashMap<>(); 
-        map.put(')', '('); 
-        map.put('}', '{');
-        map.put(']', '[');
-        
-        int n = s.length();
-        s += s; 
         int answer = 0;
-
-        A: for (int i = 0; i < n; i++) { 
+        int len = s.length();
+        String rhkfgh = s+s;
+        int start = 0;
+        int end = len;
+        
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+        
+        while(start<len) {
             ArrayDeque<Character> stack = new ArrayDeque<>();
+            stack.push(rhkfgh.charAt(start));
+            String target = rhkfgh.substring(start+1,end);
             
-            for (int j = i; j < i + n; j++) { 
-                char c = s.charAt(j);
-                
-                if (!map.containsKey(c)) {
-                    stack.push(c);
-                } else {
-                    if (stack.isEmpty() || !stack.pop().equals(map.get(c))) {
-                        continue A;
-                    }
+            for (Character t : target.toCharArray()) {
+                if (t == '(' || t == '[' || t == '{') stack.push(t);
+                else {
+                    if (!stack.isEmpty() && map.get(stack.peek()) == t) stack.pop();
                 }
             }
-                        
-            if (stack.isEmpty()) {
-                answer++;
-            }
+            
+            if (stack.isEmpty()) answer++;
+            start++;
+            end++;
         }
         
         return answer;
