dee4b58caf435ea46725102df6a109be867842ca
kgy1008 <gayeon1008@g.skku.edu>
2024-09-28 02:58:05 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int total = brown + yellow;
+        
+        for (int height = 3; height <= Math.sqrt(total); height++) {
+            int width = total/height;
+            
+            if ((width-2) * (height-2) == yellow) {
+                return new int[] {width, height};
+            }
+        }
+        return new int[0]; 
+    }
+}
