fc0434930e53b77a43c2668c21a1a36ef715f793
kgy1008 <gayeon1008@g.skku.edu>
2024-10-04 03:33:20 +0900

    [Bronze III] Title: 웰컴 키트, Time: 100 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -1,22 +1,35 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+ 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		int[] List = new int[6];
-		for (int i=0;i<6;i++) {
-			List[i]=sc.nextInt();
-		}
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int t=0;
-		for (int i=0;i<6;i++) {
-			t+=List[i]/a;
-			if (List[i]%a!=0) {
-				t++;
-			}
-		}
-		System.out.println(t);
-		System.out.println(num/b+" "+num%b);
-	}
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+        int N = Integer.parseInt(br.readLine());
+ 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] size = new int[6];
+ 
+        for(int i=0; i<size.length; i++)
+            size[i] = Integer.parseInt(st.nextToken());
+ 
+        st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());   
+        int P = Integer.parseInt(st.nextToken());   
+ 
+        int answer = 0; 
+        for (int j=0; j<6; j++) {
+            answer += size[j] / T;
+            if(size[j] % T != 0)
+                answer++;
+        }
+        System.out.println(answer);
+        StringBuilder sb = new StringBuilder();
+        sb.append(N / P).append(" ").append(N % P);
+        System.out.println(sb);
+    }
 }
