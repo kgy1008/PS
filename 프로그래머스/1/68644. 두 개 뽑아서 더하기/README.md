ca26a05f3f6e12c21cb5deda2fd0073e8d7643c0
kgy1008 <gayeon1008@g.skku.edu>
2024-10-12 00:18:59 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 4.53 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644) 
 
 ### 성능 요약
 
-메모리: 86.8 MB, 시간: 6.10 ms
+메모리: 85.5 MB, 시간: 4.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 06일 19:16:45
+2024년 10월 12일 00:18:56
 
 ### 문제 설명
 
--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,22 +1,19 @@
-import java.util.HashSet;
-import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers) {
-        HashSet<Integer> answer = new HashSet<>();
-
+        Set<Integer> ans = new HashSet<>();
+        
         Arrays.sort(numbers);
         
-        for (int i = 0; i < numbers.length - 1; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                answer.add(numbers[i] + numbers[j]);
+        for (int i=0; i<numbers.length-1; i++) {
+            for (int j=i+1; j<numbers.length; j++) {
+                ans.add(numbers[i] + numbers[j]);
             }
         }
         
-        return answer.stream()
-                     .sorted()
-                     .mapToInt(Integer::intValue)
-                     .toArray();
+        int[] answer = ans.stream().mapToInt(Integer::intValue).toArray();
+        Arrays.sort(answer);
+        return answer;
     }
 }
