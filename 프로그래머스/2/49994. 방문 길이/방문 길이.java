643c3236a7650346fa9fd03a7b6132e4fa292591
kgy1008 <gayeon1008@g.skku.edu>
2024-10-13 23:51:51 +0900

    [level 2] Title: 방문 길이, Time: 1.46 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -2,50 +2,41 @@ import java.util.*;
 
 class Solution {
     public int solution(String dirs) {
-        int x = 0; 
-        int y = 0;
+        int x=0, y=0;
+        int nx = x, ny = y;
+        HashSet<String> set = new HashSet<>();
+        StringBuffer sb;
         
-        HashSet<String> check = new HashSet<>();
-        int answer = 0;
-        
-        for (Character d : dirs.toCharArray()) {
-            int nx = x; 
-            int ny = y;
-            
-            if (d == 'U') {
-                ny = y+1;
+        for (char command : dirs.toCharArray()) {
+            if (command == 'U') {
+                ny = y+1; 
             }
-            else if (d == 'D') {
+            else if (command == 'D') {
                 ny = y-1;
             }
-            
-            else if (d == 'R') {
+            else if (command == 'R') {
                 nx = x+1;
             }
             else {
                 nx = x-1;
             }
             
-            if (nx >= -5 && nx <= 5 && ny >= -5 && ny <= 5) {
-                StringBuffer sb1 = new StringBuffer();
-                String reverse = sb1.append(nx).append(ny).append(x).append(y).toString();
-
-                StringBuffer sb2 = new StringBuffer();
-                String load = sb2.append(x).append(y).append(nx).append(ny).toString();
-
-                if (!check.contains(load)) {
-                    answer++;   
-                    check.add(load);
-                    check.add(reverse);
-                }
-
-                x = nx;
-                y = ny;
+            if (nx<-5 || nx>5 || ny<-5 || ny>5) {
+                nx = x;
+                ny = y;
+                continue;
             }
             
+            sb = new StringBuffer();
+            String start = sb.append(x).append(y).append(nx).append(ny).toString();
+            set.add(start);
+            sb = new StringBuffer();
+            String reverse = sb.append(nx).append(ny).append(x).append(y).toString();
+            set.add(reverse);
             
+            x = nx; 
+            y = ny;
         }
-        
-        return answer;
+        return set.size()/2;
     }
 }
