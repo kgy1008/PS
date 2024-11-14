7b697b98f92ef7287923d619af57f88a057f82e3
kgy1008 <gayeon1008@g.skku.edu>
2024-11-14 14:01:11 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 14404 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i=3; i<1001; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+        }
+        
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(dp[n]);
+    }
+}
