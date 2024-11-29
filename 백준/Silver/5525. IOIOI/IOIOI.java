6a1063aba25cdc4512f8a63e49b33d634fcadfe2
kgy1008 <gayeon1008@g.skku.edu>
2024-11-29 13:23:27 +0900

    [Silver I] Title: IOIOI, Time: 260 ms, Memory: 50032 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,37 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Main {
-    private static int[] num;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
-        int size = 2 * n + 1;
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < size; i++) {
-            if (i % 2 == 0) {
-                sb.append("I");
-            } else {
-                sb.append("O");
-            }
-        }
-
-        String target = sb.toString();
-
+        ArrayList<Integer> arr = new ArrayList<>();
         String s = br.readLine();
-
-        int answer = 0;
-        for (int i = 0; i <= m - size; i++) {
-            String find = s.substring(i, i + size);
-            if (find.equals(target)) {
-                answer++;
+        for (int i = 0; i < m; i++) {
+            if (s.charAt(i) == 'I') {
+                arr.add(i);
             }
         }
-        System.out.println(answer);
+
+        int interval = 0, result = 0;
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr.get(i) - arr.get(i - 1) == 2) {
+                interval++;
+            } else {
+                interval = 0;
+            }
+
+            if (interval >= n) {
+                result++;
+            }
+        }
+        System.out.println(result);
     }
 }
