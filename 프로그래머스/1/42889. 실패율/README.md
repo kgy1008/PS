39dc698d67135901537d8fc8ccac85e55667a755
kgy1008 <gayeon1008@g.skku.edu>
2024-12-23 13:56:40 +0900

    [level 1] Title: 실패율, Time: 17.13 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/README.md
+++ 프로그래머스/1/42889. 실패율/README.md
@@ -1,10 +1,10 @@
 # [level 1] 실패율 - 42889 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889?language=java) 
 
 ### 성능 요약
 
-메모리: 86.1 MB, 시간: 9.79 ms
+메모리: 80 MB, 시간: 17.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 13일 19:43:35
+2024년 12월 23일 13:56:38
 
 ### 문제 설명
 
--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,27 +1,33 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
-        int[] clear = new int[N+2];
-        int player = stages.length;
+        int[] arr = new int[N+2];
+        int person = stages.length;
         
         for (int s : stages) {
-            clear[s]++;
+            arr[s]++;
         }
         
-        HashMap<Integer, Double> fail = new HashMap<>();
-        double notClear = 0.0;
         
-        for (int i=1; i<N+1; i++) {
-            if (player == 0) {
-                fail.put(i,0.0);
+        Map<Integer, Double> detail = new HashMap<>();
+        for (int stage=1; stage<=N; stage++) {
+            if (person == 0) {
+                detail.put(stage, 0.0);
                 continue;
             }
-            notClear = clear[i];
-            fail.put(i, notClear/player);
-            player -= notClear;
+            int arrived = arr[stage];
+            double failure = (double) arrived / person;
+            detail.put(stage, failure);
+            person -= arrived;
         }
         
-        return fail.entrySet().stream().sorted((o1,o2) -> Double.compare(o2.getValue(), o1.getValue())).mapToInt(HashMap.Entry::getKey).toArray();
+        List<Integer> answer = detail.entrySet().stream()
+            .sorted((o1,o2) -> Double.compare(o2.getValue(), o1.getValue()))
+            .map(i-> i.getKey())
+            .collect(Collectors.toList());
+        
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
