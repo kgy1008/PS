72346ba3e5abe59932598cbb92ba90b0646da682
kgy1008 <gayeon1008@g.skku.edu>
2024-12-23 15:53:13 +0900

    [level 2] Title: 주식가격, Time: 33.80 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -3,18 +3,22 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
+        Deque<int[]> stack = new ArrayDeque<>();
         
         for (int i = 0; i<prices.length; i++) {
-            answer[i] = prices.length - i - 1;
+            while (!stack.isEmpty() && stack.peek()[1] > prices[i]) {
+                int[] tmp = stack.pop();
+                int idx = tmp[0];
+                answer[idx] = i - idx;
+            }
+            stack.push(new int[]{i, prices[i]});
         }
         
-        for (int i = 0; i<prices.length-1; i++) {
-            for (int j=i+1; j<prices.length; j++) {
-                if (prices[i] > prices[j]) {
-                    answer[i] = j-i;
-                    break;
-                }
-            }
+        int lastIdx = prices.length - 1;
+        while (!stack.isEmpty()) {
+            int[] tmp = stack.pop();
+            int idx = tmp[0];
+            answer[idx] = lastIdx-idx;
         }
         
         return answer;
