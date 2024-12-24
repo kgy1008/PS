27fa48d3576a6f243815fb46626baf5b732424d2
kgy1008 <gayeon1008@g.skku.edu>
2024-12-24 15:44:35 +0900

    [level 2] Title: [3차] 압축, Time: 9.21 ms, Memory: 95.2 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/README.md
+++ 프로그래머스/2/17684. ［3차］ 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] 압축 - 17684 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17684) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17684?language=java) 
 
 ### 성능 요약
 
-메모리: 77.3 MB, 시간: 3.95 ms
+메모리: 95.2 MB, 시간: 9.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 16일 13:27:44
+2024년 12월 24일 15:44:34
 
 ### 문제 설명
 
--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -1,36 +1,29 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(String msg) {
-        HashMap<String, Integer> index = new HashMap<>();
-        
-        // A-Z 알파벳 사전 초기화
-        int max = 1;
-        for (int i = 0; i < 26; i++) {
-            char letter = (char) ('A' + i);  // 'A'부터 'Z'까지 문자
-            index.put(String.valueOf(letter), max);
-            max++;
+    public int[] solution(String msg) {        
+        Map<String, Integer> map = new HashMap<>();
+        for (int i=0; i<26; i++) {
+            char tmp = (char) ('A' + i);
+            map.put(String.valueOf(tmp), i+1);
         }
         
-        int start = 0;
-        int end = start + 1;
-        ArrayList<Integer> answer = new ArrayList<>();
-
-        while (end <= msg.length()) {
-            String key = msg.substring(start, end);
-            if (!index.containsKey(key)) {
-                String prevKey = msg.substring(start, end - 1);
-                answer.add(index.get(prevKey));
-                index.put(key, max);
-                max++;
-                start = end - 1;
+        int start = 0, end = 1, last = 26;
+        int idx = 0;
+        
+        List<Integer> answer = new ArrayList<>();
+        while (start < msg.length()) {
+            String tmp = msg.substring(start, end);
+            while (end <= msg.length() && map.containsKey(tmp)) {
+                idx = map.get(tmp);
+                end++;
+                if (end <= msg.length()) {
+                    tmp = msg.substring(start, end);
+                }
             }
-                        
-            if (end >= msg.length() && index.containsKey(msg.substring(start,end))) {
-                answer.add(index.get(msg.substring(start,end)));
-                break;
-            }
-            end++;
+            map.put(tmp, ++last);
+            answer.add(idx);
+            start = end - 1;
         }
         
         return answer.stream().mapToInt(Integer::intValue).toArray();
