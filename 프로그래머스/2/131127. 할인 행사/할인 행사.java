68e25096e8ac96a40141b0360b4f2ad85a1d74b9
kgy1008 <gayeon1008@g.skku.edu>
2024-12-30 21:20:00 +0900

    [level 2] Title: 할인 행사, Time: 95.56 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,25 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
-        HashMap<String, Integer> product = new HashMap<>();
-        int day = 0;
+        Map<String, Integer> target = new HashMap<>();
         
-        for (int i = 0; i < want.length; i++) {
-            product.put(want[i], number[i]);
+        for (int i=0; i<want.length; i++) {
+            target.put(want[i], number[i]);
         }
         
-        for (int i = 0; i <= discount.length - 10; i++) {
-            HashMap<String, Integer> dis = new HashMap<>();
+        int answer = 0;
+        for (int i=0; i<=discount.length-10; i++) {
+            Map<String, Integer> basket = new HashMap<>();
+            for (int j=i; j<i+10; j++) {
+                if (target.containsKey(discount[j])) {
+                    basket.put(discount[j], basket.getOrDefault(discount[j], 0) + 1);
+                }
+            }
             
-            for (int j = i; j < i + 10; j++) {
-                dis.put(discount[j], dis.getOrDefault(discount[j], 0) + 1);
-            }
-
-            if (dis.equals(product)) {
-                day++;
+            if (basket.equals(target)) {
+                answer++;
             }
         }
-        
-        return day;
+        return answer;
     }
 }
