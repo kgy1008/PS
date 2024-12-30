bf91891711411851d57fd6acc4be323e0fcb6815
kgy1008 <gayeon1008@g.skku.edu>
2024-12-31 02:04:01 +0900

    [level 2] Title: 귤 고르기, Time: 52.68 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -2,21 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int[] tangerine) {
-        HashMap<Integer,Integer> orange = new HashMap<>();
-        int answer = 0;
-        
-        for (int cnt : tangerine) {
-            orange.put(cnt,orange.getOrDefault(cnt,0) + 1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int t : tangerine) {
+            map.put(t, map.getOrDefault(t, 0) + 1);
         }
         
-        List<Integer> count = new ArrayList<>(orange.values());
-        Collections.sort(count, Comparator.reverseOrder());
+        List<Integer> sortedCount = new ArrayList<>(map.values());
+        sortedCount.sort((o1,o2) -> Integer.compare(o2,o1));
         
+        int answer = 0;
+        int sum = 0;
         
-        for(int o : count) {
-            if (k <= 0) break;
-            k -= o;
+        for (int count : sortedCount) {
+            sum += count;
             answer++;
+            
+            if (sum >= k) {
+                break;
+            }
         }
         
         return answer;
