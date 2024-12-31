bbb82bc8ffdb45f94bfb3c199cc1e88d055b1f85
kgy1008 <gayeon1008@g.skku.edu>
2024-12-31 17:26:05 +0900

    [level 1] Title: 신고 결과 받기, Time: 740.31 ms, Memory: 178 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,45 +1,41 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        LinkedHashMap<String, Set<String>> warnList = new LinkedHashMap<>();
-        HashMap<String, Integer> warnCount = new HashMap<>();
+        Map<String, List<String>> map = new LinkedHashMap<>();
         
-        for (String name : id_list) {
-            warnList.put(name, new HashSet<String>());
+        for (String id : id_list) {
+            map.put(id, new ArrayList<>());
         }
+        
+        Map<String, Integer> warnCount = new HashMap<>();
         
         for (String r : report) {
-            String[] reportName = r.split(" ");
-            
-            Set<String> nameSet = warnList.get(reportName[0]);
-            if (!nameSet.contains(reportName[1])) {
-                nameSet.add(reportName[1]);
-                warnList.put(reportName[0], nameSet);
-            
-                warnCount.put(reportName[1], warnCount.getOrDefault(reportName[1], 0) + 1);
+            String[] info = r.split(" ");
+            List<String> list = map.get(info[0]);
+            if (!list.contains(info[1])) {
+                list.add(info[1]);
+                warnCount.put(info[1], warnCount.getOrDefault(info[1], 0) + 1);
             }
         }
         
-        ArrayList<String> stopPlayer = new ArrayList<>();
-        
-        for (String player : warnCount.keySet()) {
-            if (warnCount.get(player) >= k) {
-                stopPlayer.add(player);
-            }
-        }
-        
-        stopPlayer.stream().forEach(System.out::println);
+        List<String> out = warnCount.entrySet().stream()
+            .filter(o-> o.getValue() >= k)
+            .map(o -> o.getKey())
+             .collect(Collectors.toList());
         
         int[] result = new int[id_list.length];
-        int idx = 0;
         
-        for (String reporter : warnList.keySet()) {
-            Set<String> r = warnList.get(reporter);
-            for (String sp : stopPlayer) {
-                if (r.contains(sp)) result[idx]++;
+        for (int i=0; i<id_list.length; i++) {
+            int cnt = 0;
+            List<String> target = map.get(id_list[i]);
+            for (String o : out) {
+                if (target.contains(o)) {
+                    cnt++;
+                }
             }
-            idx++;
+            result[i] = cnt;
         }
         
         return result;
