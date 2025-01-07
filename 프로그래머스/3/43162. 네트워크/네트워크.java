a992f8d01d559392897023fc41c968fe24755340
kgy1008 <gayeon1008@g.skku.edu>
2025-01-07 16:02:06 +0900

    [level 3] Title: 네트워크, Time: 1.31 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,31 +1,46 @@
-public class Solution {
+import java.util.*;
 
-    private static boolean[] visit;
-    private static int[][] computer;
-
-    private static void dfs(int now) {
-        visit[now] = true; // ❶ 현재 노드 방문 처리
-        for (int i = 0; i < computer[now].length; i++) {
-            // ❷ 연결되어 있으며 방문하지 않은 노드라면
-            if (computer[now][i] == 1 && !visit[i]) {
-                dfs(i); // ❸ 해당 노드를 방문하러 이동
-            }
-        }
-    }
-
+class Solution {
+    private static List<Integer>[] node;
+    private static boolean[] visited;
+    
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        computer = computers;
-        visit = new boolean[n]; // ❹ 방문 여부를 저장할 배열
-
-        for (int i = 0; i < n; i++) {
-            if (!visit[i]) { // ❺ 아직 방문하지 않은 노드라면 해당 노드를 시작으로 깊이 우선 탐색 진행
-                dfs(i);
-                answer++; // ❻ DFS로 연결된 노드들을 모두 방문하면서 네트워크 개수 증가
+        node = new ArrayList[n];
+        visited = new boolean[n];
+        for (int i=0; i<n; i++) {
+            node[i] = new ArrayList<>();
+        }
+        
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                if (i != j && computers[i][j] == 1) {
+                    node[i].add(j);
+                }
             }
         }
-
+        
+        int answer = 0;
+        for (int i=0; i<n; i++) {
+            if (!visited[i] && node[i].isEmpty()) {
+                answer++;
+            }
+            for (int j : node[i]) {
+                if (!visited[j]) {
+                    dfs(j);
+                    answer++;
+                }
+            }
+        }
         return answer;
     }
-
+    
+    private static void dfs(int start) {
+        visited[start] = true;
+        
+        for (int next : node[start]) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
 }
