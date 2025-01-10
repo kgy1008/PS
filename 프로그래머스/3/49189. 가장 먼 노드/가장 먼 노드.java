4452265e49ce5cd7a79c72a97359907c5e50694d
kgy1008 <gayeon1008@g.skku.edu>
2025-01-10 12:49:56 +0900

    [level 3] Title: 가장 먼 노드, Time: 49.22 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,61 +2,48 @@ import java.util.*;
 
 class Solution {
     static ArrayList<Integer>[] adj;
-    static int[] cost;
-    
+
     public int solution(int n, int[][] edge) {
-        adj = new ArrayList[n+1];
-        for (int i=1; i<=n; i++) {
+        adj = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
             adj[i] = new ArrayList<>();
         }
-        
         for (int[] e : edge) {
             adj[e[0]].add(e[1]);
             adj[e[1]].add(e[0]);
         }
-        
-        cost = new int[n+1];
-        Arrays.fill(cost, 987654321);
-        cost[0] = cost[1] = 0;
-        dij();
-        
-        int max = Arrays.stream(cost).max().getAsInt();
-        int answer=0;
-        for (int i=2; i<=n; i++) {
-            if (cost[i] == max) {
+
+        int[] dist = bfs(n);
+
+        int max = Arrays.stream(dist).max().getAsInt();
+        int answer = 0;
+        for (int d : dist) {
+            if (d == max) {
                 answer++;
             }
         }
         return answer;
     }
-    
-    static void dij() {
-        PriorityQueue<Node> queue = new PriorityQueue<>(Comparator.comparingInt(o -> o.cost));
-        queue.offer(new Node(1, 0));
-        
+
+    static int[] bfs(int n) {
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[n + 1];
+        int[] dist = new int[n + 1];
+
+        queue.offer(1);
+        visited[1] = true;
+
         while (!queue.isEmpty()) {
-            Node cur = queue.poll();
-            
-            if (cost[cur.num] < cur.cost) {
-                continue;
-            }
-            
-            for (int next : adj[cur.num]) {
-                if (cost[next] > cur.cost + 1) {
-                    cost[next] = cur.cost+1;
-                    queue.offer(new Node(next, cost[next]));
+            int cur = queue.poll();
+
+            for (int next : adj[cur]) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    dist[next] = dist[cur] + 1; 
+                    queue.offer(next);
                 }
             }
         }
-    }
-    
-    static class Node {
-        int num;
-        int cost;
-        
-        Node(int num, int cost) {
-            this.num = num;
-            this.cost = cost;
-        }
+        return dist;
     }
 }
