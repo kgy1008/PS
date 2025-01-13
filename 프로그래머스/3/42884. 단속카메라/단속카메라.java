f0928e48966fafae8704530f44da846e889337a6
kgy1008 <gayeon1008@g.skku.edu>
2025-01-13 15:11:49 +0900

    [level 3] Title: 단속카메라, Time: 10.79 ms, Memory: 55.7 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,26 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
+        Arrays.sort(routes, (o1,o2) -> Integer.compare(o1[1], o2[1]));
+        
         int answer = 0;
-        
-        Arrays.sort(routes, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] route1, int[] route2) {
-                return route1[1] - route2[1];
-            }
-        });
-        
         int cam = Integer.MIN_VALUE;
         
         for(int[] route : routes) {
             if(cam < route[0]) {
-                // 현재 카메라의 위치가 route의 시작 지점보다 작으면
-                // 새로운 cam을 route의 종료 지점에 설치한다
                 cam = route[1];
                 answer++;
             }
         }
-        
         return answer;
     }
 }
