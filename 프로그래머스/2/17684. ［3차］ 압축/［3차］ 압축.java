dff4ea1e93dcdd2bec38688dfd8d8cb776881693
kgy1008 <gayeon1008@g.skku.edu>
2025-01-14 14:54:00 +0900

    [level 2] Title: [3차] 압축, Time: 3.67 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -1,31 +1,27 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(String msg) {        
+    public int[] solution(String msg) {
         Map<String, Integer> map = new HashMap<>();
-        for (int i=0; i<26; i++) {
-            char tmp = (char) ('A' + i);
-            map.put(String.valueOf(tmp), i+1);
-        }
         
-        int start = 0, end = 1, last = 26;
-        int idx = 0;
+        for (int i=0; i<26; i++) {
+            char alpha = (char) ('A' + i);
+            map.put(String.valueOf(alpha), i+1);
+        }
+        int lastIdx = 26;
+        int idx = 0, end = 1;
         
         List<Integer> answer = new ArrayList<>();
-        while (start < msg.length()) {
-            String tmp = msg.substring(start, end);
-            while (end <= msg.length() && map.containsKey(tmp)) {
-                idx = map.get(tmp);
+        while (idx < msg.length()) {
+            while (end <= msg.length() && map.containsKey(msg.substring(idx,end))) {
                 end++;
-                if (end <= msg.length()) {
-                    tmp = msg.substring(start, end);
-                }
             }
-            map.put(tmp, ++last);
-            answer.add(idx);
-            start = end - 1;
+            if (end <= msg.length()) {
+                map.put(msg.substring(idx,end), ++lastIdx);
+            }
+            answer.add(map.get(msg.substring(idx,end-1)));
+            idx = end - 1;
         }
-        
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
