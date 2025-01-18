1f0fd27143070aa02e82f5eeb6f985af1ae52852
kgy1008 <gayeon1008@g.skku.edu>
2025-01-18 15:57:26 +0900

    [level 2] Title: 타겟 넘버, Time: 5.35 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,17 +1,14 @@
 class Solution {
     static int answer = 0;
-    static int sum = 0;
-    static boolean[] visited;
     static int[] nums;
     
     public int solution(int[] numbers, int target) {
-        visited = new boolean[numbers.length];
         nums = numbers;
-        dfs(0, target);
+        dfs(0, target, 0);
         return answer;
     }
     
-    static void dfs(int degree, int target) {
+    static void dfs(int degree, int target, int sum) {
         if (degree == nums.length) {
             if (sum == target) {
                 answer++; 
@@ -19,16 +16,7 @@ class Solution {
             return;
         }
         
-        for (int i=0; i<2; i++) {
-            if (i == 0) {
-                sum += nums[degree];
-                dfs(degree+1, target);
-                sum -= nums[degree];
-            } else {
-                sum += nums[degree] * -1;
-                dfs(degree+1, target);
-                sum -= nums[degree] * -1;
-            }
-        }
+        dfs(degree+1, target, sum + nums[degree]);
+        dfs(degree+1, target, sum - nums[degree]);
     }
 }
