fddbfd10981880201fb6baef4389209b290f8a29
kgy1008 <gayeon1008@g.skku.edu>
2025-01-20 15:00:02 +0900

    [level 2] Title: 모음 사전, Time: 7.37 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,15 +1,14 @@
 import java.util.*;
 
 class Solution {
-    static List<String> list;
+    static List<String> list = new ArrayList<>();
     static String [] words = {"A", "E", "I", "O", "U"};
     
     public int solution(String word) {
-        int answer = 0;
-        list = new ArrayList<>();
         dfs("", 0);
-        int size = list.size();
-        for (int i = 0; i < size; i++) {
+
+        int answer = 0;
+        for (int i = 0; i < list.size(); i++) {
             if (list.get(i).equals(word)) {
                 answer = i;
                 break;
