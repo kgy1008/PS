c91de195e8d6999e4a815cc5295c8eb514a7f6e8
kgy1008 <gayeon1008@g.skku.edu>
2025-01-25 16:11:20 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 66.45 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,35 +1,35 @@
 import java.util.*;
 
 class Solution {
-
-    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        HashMap<String, String> user = new HashMap<>();
-        HashMap<String, Integer> sell = new HashMap<>();
-        int[] answer = new int[enroll.length];
-
+    static Map<String, Integer> profit = new HashMap<>();
+    static Map<String, String> parent = new HashMap<>();
+    
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {        
         for (int i=0; i<enroll.length; i++) {
-            user.put(enroll[i], referral[i]);
+            parent.put(enroll[i], referral[i]); // enroll[i]의 부모는 referall[i]
+            profit.put(enroll[i], 0);
         }
-
+        
         for (int i=0; i<seller.length; i++) {
-            String s = seller[i];
             int money = amount[i] * 100;
-            distributeProfit(s, money, user, sell);
+            backtrack(seller[i], money);
         }
-
+        
+        int[] answer = new int[enroll.length];
         for (int i=0; i<enroll.length; i++) {
-            answer[i] = sell.getOrDefault(enroll[i], 0);
-        }
-
+            answer[i] = profit.get(enroll[i]);
+        } 
         return answer;
     }
-
-    private void distributeProfit(String s, int money, HashMap<String, String> user, HashMap<String, Integer> sell) {
-        if (s.equals("-") || money == 0) return;
-
-        int profitToKeep = money - (money / 10);
-        sell.put(s, sell.getOrDefault(s, 0) + profitToKeep);
-
-        distributeProfit(user.get(s), money / 10, user, sell);
+    
+    private static void backtrack(String person, int money) {
+        if (person.equals("-") || money < 10) {
+            profit.put(person, profit.getOrDefault(person, 0) + money);
+            return;
+        }
+        int minus = (int) Math.floor(money * 0.1);
+        int total = money - minus;
+        profit.put(person, profit.get(person) + total);
+        backtrack(parent.get(person), minus);
     }
 }
