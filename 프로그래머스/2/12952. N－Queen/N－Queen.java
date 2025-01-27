0e78cafb45ec460c6ce4635f0076bf15587d2681
kgy1008 <gayeon1008@g.skku.edu>
2025-01-27 23:08:07 +0900

    [level 2] Title: N-Queen, Time: 116.13 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,26 +1,46 @@
 class Solution {
-    boolean[] width;
-    boolean[] diagonal1;
-    boolean[] diagonal2;
+    static boolean[] dialog1;
+    static boolean[] dialog2;
+    static boolean[] rkfh;
+    static int answer = 0;
     
     public int solution(int n) {
-        width = new boolean[n];
-        diagonal1 = new boolean[n*2];
-        diagonal2 = new boolean[n*2];
-        return getAnswer(0, n);
+        dialog1 = new boolean[2*n];
+        dialog2 = new boolean[2*n];
+        rkfh = new boolean[n];
+        
+        solve(0, n);
+        return answer;
     }
     
-    private int getAnswer(int y, int n) {
-        int ans = 0;
-        if (y == n) ans++;
-        else {
-            for (int i = 0; i<n; i++) {
-                if (width[i] || diagonal1[i+y] || diagonal2[i-y+n]) continue;
-                width[i] = diagonal1[i+y] = diagonal2[i-y+n] = true;
-                ans += getAnswer(y+1, n);
-                width[i] = diagonal1[i+y] = diagonal2[i-y+n] = false;
+    private static void solve(int row, int n) {
+        if (row == n) {
+            answer++;
+            return;
+        }
+        for (int i=0; i<n; i++) {
+            if (isValid(row, i, n)) {
+                dialog1[row+i] = true;
+                dialog2[row-i+n] = true;
+                rkfh[i] = true;
+                solve(row+1, n);
+                dialog1[row+i] = false;
+                dialog2[row-i+n] = false;
+                rkfh[i] = false;
             }
         }
-        return ans;
+    }
+    
+    private static boolean isValid(int row, int col, int n) {
+        if (dialog1[row+col]) {
+            return false;
+        }
+        if (dialog2[row-col+n]) {
+            return false;
+        }
+        if (rkfh[col]) {
+            return false;
+        }
+        return true;
     }
 }
