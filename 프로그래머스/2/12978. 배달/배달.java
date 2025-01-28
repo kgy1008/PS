b407cff4246af3ad7196f93692194eb8ab3905a5
kgy1008 <gayeon1008@g.skku.edu>
2025-01-28 13:45:01 +0900

    [level 2] Title: 배달, Time: 10.34 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -2,55 +2,48 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int[][] road, int K) {
-        int[] dist = new int[N+1];
+        int answer = 0;
         
-        List<Node>[] adj = new ArrayList[N+1];
-        for (int i=1; i<N+1; i++) {
+        List<Node>[] adj = new ArrayList[N + 1];
+        for (int i = 1; i < N + 1; i++) {
             adj[i] = new ArrayList<>();
-            dist[i] = 987654321;
         }
-        
+        for (int[] r : road) {
+            adj[r[0]].add(new Node(r[1], r[2]));
+            adj[r[1]].add(new Node(r[0], r[2]));
+        }
+
+        int[] dist = new int[N + 1];
+        Arrays.fill(dist, 987654321); 
         dist[1] = 0;
 
-        for (int[] r : road) {
-            int a = r[0];
-            int b = r[1];
-            int c = r[2];
-            adj[a].add(new Node(b,c));
-            adj[b].add(new Node(a,c));
-        }
-        
-        PriorityQueue<Node> queue = new PriorityQueue<>(Comparator.comparingInt(o1 -> o1.cost));
-        queue.add(new Node(1, 0));
+        PriorityQueue<Node> queue = new PriorityQueue<>((o1,o2) -> Integer.compare(o1.cost, o2.cost));
+        queue.offer(new Node(1,0));
         
         while (!queue.isEmpty()) {
-            Node current = queue.poll();
-            if (dist[current.num] < current.cost) {
-                continue;
-            }
+            Node cur = queue.poll();
             
-            for (Node next : adj[current.num]) {
-                if (dist[next.num] > current.cost + next.cost) {
-                    dist[next.num] = current.cost + next.cost;
+            for (Node next : adj[cur.num]) {
+                if (dist[next.num] > next.cost + dist[cur.num]) {
+                    dist[next.num] = next.cost + dist[cur.num];
                     queue.offer(new Node(next.num, dist[next.num]));
                 }
             }
         }
         
-        int answer = 0;
-        for (int i=1; i<N+1; i++) {
-            if (dist[i] <= K) {
+        for (int d : dist) {
+            if (d <= K) {
                 answer++;
             }
         }
         return answer;
     }
     
-    private static class Node {
+    static class Node {
         int num;
         int cost;
         
-        public Node(int num, int cost) {
+        Node (int num, int cost) {
             this.num = num;
             this.cost = cost;
         }
