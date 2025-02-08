1fa796c976655cf55f3613f17ab59ccb44eff6bf
kgy1008 <gayeon1008@g.skku.edu>
2025-02-08 21:37:07 +0900

    [Silver I] Title: 절댓값 힙, Time: 292 ms, Memory: 24748 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -4,25 +4,33 @@ import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
 public class Main {
+    static char[] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        PriorityQueue<Integer> que = new PriorityQueue<>((o1, o2) -> {
-            int absCompare = Integer.compare(Math.abs(o1), Math.abs(o2));
-            if (absCompare == 0) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+            int result = Integer.compare(Math.abs(o1), Math.abs(o2));
+            if (result == 0) {
                 return Integer.compare(o1, o2);
             }
-            return absCompare;
+            return result;
         });
-        StringBuilder sb = new StringBuilder();
 
+        StringBuilder sb = new StringBuilder();
         while (n-- > 0) {
             int num = Integer.parseInt(br.readLine());
-            if (num == 0) {
-                sb.append(que.isEmpty() ? 0 : que.poll()).append('\n');
-            } else {
-                que.add(num);
+
+            if (num != 0) {  // 추가
+                pq.add(num);
+            } else {  // 절대값이 가장 작은 값 제거 및 출력
+                if (pq.isEmpty()) {
+                    sb.append("0\n");
+                    continue;
+                }
+                int temp = pq.poll();
+                sb.append(temp).append("\n");
             }
         }
         System.out.println(sb);
