8fafbf8c50ccfb29d0e3e3fe92e821061953a4e5
kgy1008 <gayeon1008@g.skku.edu>
2025-02-08 20:25:10 +0900

    [Silver IV] Title: 스택, Time: 256 ms, Memory: 20300 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		Deque<String> stack = new ArrayDeque<>();
+		while (n --> 0) {
+			String s = br.readLine();
+			if (s.equals("size")) {
+				System.out.println(stack.size());
+			} else if (s.equals("empty")) {
+				System.out.println(stack.isEmpty() ? 1 : 0);
+			} else if (s.equals("top")) {
+				if (stack.isEmpty()) {
+					System.out.println(-1);
+				} else {
+					System.out.println(stack.peek());
+				}
+			} else if (s.equals("pop")){
+				if (stack.isEmpty()) {
+					System.out.println(-1);
+				} else {
+					System.out.println(stack.peek());
+					stack.pop();
+				}
+			} else {
+				String[] arr = s.split(" ");
+				stack.push(arr[1]);
+			}
+		}
+	}
+
+}
