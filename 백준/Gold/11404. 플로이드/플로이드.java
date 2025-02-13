634950df2a878a78561ac0d10e622a3b6f1c3dca
kgy1008 <gayeon1008@g.skku.edu>
2025-02-13 20:47:37 +0900

    [Gold IV] Title: 플로이드, Time: 336 ms, Memory: 44164 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,54 +1,63 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    private static final int MAX_VALUE = 100_000_00;
+	static final int INF = 100_000 * 100 + 1;
+	static int[][] dist;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());  // 도시의 개수
+		int m = Integer.parseInt(br.readLine());  // 버스의 개수
+		// 거리 초기화
+		dist = new int[n+1][n+1];
+		for (int i=1; i<=n; i++) {
+			for (int j=1; j<=n; j++) {
+				if (i == j) {
+					dist[i][j] = 0;
+				} else {
+					dist[i][j] = INF;
+				}
+			}
+		}
+		
+		while (m--> 0) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			dist[a][b] = Math.min(dist[a][b], c);
+		}
+		
+		for (int k=1; k<=n; k++) {  // 경유지
+			for (int i=1; i<=n; i++) {
+				for (int j=1; j<=n; j++) {
+					if (dist[i][k] == INF || dist[k][j] == INF) {
+						continue;
+					}
+					dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+				}
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i=1; i<=n; i++) {
+			for (int j=1; j<=n; j++) {
+				if (dist[i][j] >= INF) {
+					sb.append(0).append(" ");
+				} else {
+					sb.append(dist[i][j]).append(" ");
+				}
+			}
+			sb.append("\n");
+		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+	}
 
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-        int[][] city = new int[n+1][n+1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i==j) {
-                    city[i][j] = 0;
-                    continue;
-                }
-                city[i][j] = MAX_VALUE;
-            }
-        }
-
-        while (m --> 0) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            city[a][b] = Math.min(city[a][b], c);
-        }
-
-        for (int k=1; k<n+1; k++) {
-            for (int i=1; i<n+1; i++) {
-                for (int j=1; j<n+1; j++) {
-                    city[i][j] = Math.min(city[i][j], city[i][k] + city[k][j]);
-                }
-            }
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i=1; i<n+1; i++) {
-            for (int j=1; j<n+1; j++) {
-                if (city[i][j] >= MAX_VALUE) {
-                    city[i][j] = 0;
-                }
-                sb.append(city[i][j]).append(" ");
-            }
-            sb.append("\n");
-        }
-        System.out.println(sb);
-    }
 }
