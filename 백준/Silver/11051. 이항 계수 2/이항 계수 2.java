1b2d7e6bcae38de451f11e06c79ed5057a1deead
kgy1008 <gayeon1008@g.skku.edu>
2025-02-15 21:27:55 +0900

    [Silver II] Title: 이항 계수 2, Time: 84 ms, Memory: 19444 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        long answer = solve(n, k);
+        System.out.println(answer);
+
+        br.close();
+    }
+
+    static long solve(int n, int k) {
+        long[][] dp = new long[n + 1][k + 1];
+        int mod = 10007;
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= Math.min(i, k); j++) {
+                if (j == 0 || j == i) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % mod;
+                }
+            }
+        }
+
+        return dp[n][k];
+    }
+}
