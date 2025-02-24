f1082d3bbccad08ef41d6c735e0464d5a4faa9b7
kgy1008 <gayeon1008@g.skku.edu>
2025-02-24 13:37:12 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 53.91 ms, Memory: 210 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,37 +1,44 @@
 class Solution {
     public int solution(int[][] board, int[][] skill) {
-        int[][] sum = new int[board.length + 1][board[0].length + 1];
-
+        int n = board.length;
+        int m = board[0].length;
+        
+        int[][] diff = new int[n+1][m+1];
+        
         for (int[] s : skill) {
-            int degree = s[5] * (s[0] == 1 ? -1 : 1);
-            sum[s[1]][s[2]] += degree;
-            sum[s[3] + 1][s[4] + 1] += degree;
-            sum[s[1]][s[4] + 1] -= degree;
-            sum[s[3] + 1][s[2]] -= degree;
+            int type = s[0] == 1 ? -1 : 1; 
+            int r1 = s[1], c1 = s[2], r2 = s[3], c2 = s[4];
+            int degree = s[5] * type;
+            
+            diff[r1][c1] += degree;
+            diff[r1][c2+1] -= degree;
+            diff[r2+1][c1] -= degree;
+            diff[r2+1][c2+1] += degree;
         }
-
-        for (int i = 0; i < sum.length; i++) {
-            for (int j = 1; j < sum[0].length; j++) {
-                sum[i][j] += sum[i][j - 1];
+        
+        // 행 기준 누적 합
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                diff[i][j] += diff[i][j - 1];
             }
         }
 
-        for (int i = 1; i < sum.length; i++) {
-            for (int j = 0; j < sum[0].length; j++) {
-                sum[i][j] += sum[i - 1][j];
+        // 열 기준 누적 합
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                diff[i][j] += diff[i - 1][j];
             }
         }
-
+        
         int answer = 0;
-
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] + sum[i][j] > 0)
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                board[i][j] += diff[i][j];
+                if (board[i][j] > 0) {
                     answer++;
+                }
             }
         }
-
         return answer;
     }
-
 }
