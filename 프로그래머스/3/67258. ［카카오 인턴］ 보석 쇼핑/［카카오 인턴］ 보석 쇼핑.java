b14b448db036414bd0bf3c9da1c947e6e1cee85f
kgy1008 <gayeon1008@g.skku.edu>
2025-02-24 12:02:49 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 69.81 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -2,32 +2,35 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] gems) {
-        Set<String> gemSet = new HashSet<>(Arrays.asList(gems));
-        int kind = gemSet.size();  
+        Set<String> set = new HashSet<>(Arrays.asList(gems));
+        int total = set.size(); // 전체 보석 종류 수
         
-        Map<String, Integer> gemMap = new HashMap<>();
-        int[] answer = new int[2];
-        int minLen = Integer.MAX_VALUE;  
-        int start = 0;  
+        int start = 0, end = 0;
+        int min = gems.length - 1; 
+        int left = 1, right = gems.length;
         
-        for (int end = 0; end < gems.length; end++) {
-            gemMap.put(gems[end], gemMap.getOrDefault(gems[end], 0) + 1);
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        while (end < gems.length) {
+            map.put(gems[end], map.getOrDefault(gems[end], 0) + 1);
+            end++;
             
-            while (gemMap.size() == kind) {
-                if (end - start + 1 < minLen) {
-                    minLen = end - start + 1;
-                    answer[0] = start + 1;  
-                    answer[1] = end + 1;    
+            while (map.size() == total) { // 모든 보석이 포함될 때
+                if (end - start < min) {
+                    min = end - start;
+                    left = start + 1;
+                    right = end;
                 }
                 
-                gemMap.put(gems[start], gemMap.get(gems[start]) - 1);
-                if (gemMap.get(gems[start]) == 0) {
-                    gemMap.remove(gems[start]);
+                // 시작점 보석을 하나 제거
+                map.put(gems[start], map.get(gems[start]) - 1);
+                if (map.get(gems[start]) == 0) {
+                    map.remove(gems[start]);
                 }
-                start++;  
+                start++;
             }
         }
         
-        return answer;
+        return new int[]{left, right};
     }
 }
