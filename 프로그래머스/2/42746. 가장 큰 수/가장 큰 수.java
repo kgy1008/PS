8e7d45b3576ef3cadb2ff9820ce89fd22855eab4
kgy1008 <gayeon1008@g.skku.edu>
2025-02-27 14:01:19 +0900

    [level 2] Title: 가장 큰 수, Time: 283.75 ms, Memory: 226 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,26 +2,15 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        List<String> list = new ArrayList<>();
-        for (int number : numbers) {
-            list.add(String.valueOf(number));
-        }
+        Integer[] nums = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
         
-        list.sort((o1,o2) -> {
-            int a = Integer.parseInt(o1 + o2);
-            int b = Integer.parseInt(o2 + o1);
-            return Integer.compare(b,a);
-        });
-        
+        Arrays.sort(nums, (o1, o2) -> (o2 + "" + o1).compareTo(o1 + "" + o2));
+
         StringBuilder sb = new StringBuilder();
-        
-        for (String s : list) {
-            sb.append(s);
+        for (int num : nums) {
+            sb.append(num);
         }
-        
-        if (sb.charAt(0) == '0') {
-            return "0";
-        }
-        return sb.toString();
+
+        return sb.charAt(0) == '0' ? "0" : sb.toString();
     }
 }
