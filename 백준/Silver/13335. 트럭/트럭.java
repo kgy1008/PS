3867cda5412a74b29dacaea60a35020318ff6ffd
kgy1008 <gayeon1008@g.skku.edu>
2025-02-27 23:15:54 +0900

    [Silver I] Title: 트럭, Time: 76 ms, Memory: 11872 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.java
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -2,43 +2,45 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int w = Integer.parseInt(st.nextToken());
-        int l = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        ArrayDeque<Integer> truck = new ArrayDeque<>();
+        int n = Integer.parseInt(st.nextToken()); // 트럭의 수
+        int w = Integer.parseInt(st.nextToken()); // 다리 길이
+        int l = Integer.parseInt(st.nextToken()); // 다리 최대 하중
+
+        Deque<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < w; i++) {
+            queue.offer(0);
+        }
+
+        int[] truck = new int[n];
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            truck.offer(Integer.parseInt(st.nextToken()));
+            truck[i] = Integer.parseInt(st.nextToken());
         }
 
         int time = 0;
+        int idx = 0;
         int weight = 0;
-        ArrayDeque<Integer> tunnel = new ArrayDeque<>();
-        for(int i =0; i<w ; i++) {
-            tunnel.offer(0);
+        while (idx < n) {
+            weight -= queue.poll();
+            if (weight + truck[idx] <= l) {
+                weight += truck[idx];
+                queue.offer(truck[idx++]);
+            } else {
+                queue.offer(0);
+            }
+            time++;
         }
 
-        while(!tunnel.isEmpty()) {
-            time++;
-            weight -= tunnel.poll();
-            if(!truck.isEmpty()) {
-                if(truck.peek() + weight <= l) {
-                    weight+=truck.peek();
-                    tunnel.offer(truck.poll());
-                }
-                else{
-                    tunnel.offer(0);
-                }
-            }
-        }
-        System.out.println(time);
+        System.out.println(time + queue.size());
     }
 }
