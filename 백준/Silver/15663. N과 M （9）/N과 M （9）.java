fb16de39daa79ae3a9054af1376d8467fe7db915
kgy1008 <gayeon1008@g.skku.edu>
2025-02-27 23:46:24 +0900

    [Silver II] Title: N과 M (9), Time: 284 ms, Memory: 31892 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+    static int[] nums;
+    static boolean[] visited;
+    static Set<List<Integer>> selected = new LinkedHashSet<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        nums = new int[n];
+        visited = new boolean[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+
+        combination(m, new ArrayList<>());
+
+        StringBuilder sb = new StringBuilder();
+        for (List<Integer> numbers : selected) {
+
+            for (int num : numbers) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    static void combination(int depth, List<Integer> list) {
+        if (depth == 0) {
+            selected.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                list.add(nums[i]);
+
+                combination(depth - 1, list);
+
+                visited[i] = false;
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+}
