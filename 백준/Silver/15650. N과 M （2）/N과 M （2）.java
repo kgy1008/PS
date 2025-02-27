26fe3b85674c8ff6454fd32b4c3cd86ffbca28f6
kgy1008 <gayeon1008@g.skku.edu>
2025-02-27 23:25:32 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,36 +1,55 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, M;
-    static ArrayList<Integer> sequence = new ArrayList<>();
+    static List<List<Integer>> list = new ArrayList<>();
+    static int[] nums;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
 
-        combine(1);
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = i + 1;
+        }
+
+        combination(m, 0, new ArrayList<>());
+
+        StringBuilder sb = new StringBuilder();
+        for (List<Integer> numbers : list) {
+
+            for (int num : numbers) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
     }
 
-    public static void combine(int start) {
-        if (sequence.size() == M) {
-            for (int num : sequence) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
+    static void combination(int m, int idx, List<Integer> number) {
+        if (m == 0) {
+            list.add(new ArrayList<>(number));
             return;
         }
 
-        for (int i = start; i <= N; i++) {
-            sequence.add(i);
-            combine(i + 1);
-            sequence.remove(sequence.size() - 1);
+        for (int i = idx; i < nums.length; i++) {
+            number.add(nums[i]);
+            combination(m - 1, i + 1, number);
+            number.remove(number.size() - 1);
         }
     }
 }
