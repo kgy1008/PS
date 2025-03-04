3050a81a883b0bfd789645bf1d02c2ccd969350b
kgy1008 <gayeon1008@g.skku.edu>
2025-03-04 16:15:47 +0900

    [Gold V] Title: 치킨 배달, Time: 188 ms, Memory: 18500 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -2,74 +2,73 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N;
     static List<Node> chickens = new ArrayList<>();
-    static int[][] map;
-    static int n, m;
-    static Set<Node> home = new HashSet<>();
-    static int answer = Integer.MAX_VALUE;
+    static List<Node> homes = new ArrayList<>();
+    static int minDistance = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        map = new int[n][n];
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); // 도시의 크기 N * N
+        int m = Integer.parseInt(st.nextToken()); // 치킨 집의 개수
 
-        for (int i = 0; i < n; i++) {
+        int[][] city = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == 2) {
+            for (int j = 0; j < N; j++) {
+                city[i][j] = Integer.parseInt(st.nextToken());
+                if (city[i][j] == 2) {
                     chickens.add(new Node(i, j));
-                } else if (map[i][j] == 1) {
-                    home.add(new Node(i, j));
+                } else if (city[i][j] == 1) {
+                    homes.add(new Node(i, j));
                 }
             }
         }
 
-        combination(0, 0, new HashSet<>());
-
-        System.out.println(answer);
+        solve(m, 0, new ArrayList<>());
+        System.out.println(minDistance);
     }
 
-    static void combination(int idx, int count, Set<Node> selected) {
-        if (count == m) {
-            answer = Math.min(answer, getMinChickenDist(selected));
+    static void solve(int m, int idx, List<Node> combi) {
+        if (m == 0) {
+            calculateDistance(new ArrayList<>(combi));
             return;
         }
 
         for (int i = idx; i < chickens.size(); i++) {
-            selected.add(chickens.get(i));
-            combination(i + 1, count + 1, selected);
-            selected.remove(chickens.get(i));
+            combi.add(chickens.get(i));
+            solve(m - 1, i + 1, combi);
+            combi.remove(combi.size() - 1);
         }
     }
 
-    static int getMinChickenDist(Set<Node> selected) {
-        int totalDist = 0;
+    static void calculateDistance(List<Node> chickens) {
+        int total = 0;
+        for (int i = 0; i < homes.size(); i++) {
+            Node cur = homes.get(i);
+            int distance = Integer.MAX_VALUE;
 
-        for (Node h : home) {
-            int minDist = Integer.MAX_VALUE;
-            for (Node c : selected) {
-                minDist = Math.min(minDist, Math.abs(h.x - c.x) + Math.abs(h.y - c.y));
+            for (Node chicken : chickens) {
+                distance = Math.min(distance, Math.abs(cur.x - chicken.x) + Math.abs(cur.y - chicken.y));
             }
-            totalDist += minDist;
+            total += distance;
         }
-
-        return totalDist;
+        minDistance = Math.min(minDistance, total);
     }
 
     static class Node {
-        int x, y;
+        int x;
+        int y;
 
-        public Node(int x, int y) {
+        public Node(final int x, final int y) {
             this.x = x;
             this.y = y;
         }
