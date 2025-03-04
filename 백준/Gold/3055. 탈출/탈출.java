9c3f1da88145bbd7ef75a4c87d418c1f98b95cb8
kgy1008 <gayeon1008@g.skku.edu>
2025-03-04 15:01:55 +0900

    [Gold IV] Title: 탈출, Time: 108 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -1,86 +1,87 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static int[] dx = {-1, 1, 0, 0};
-    public static int[] dy = {0, 0, -1, 1};
-    public static Queue<int[]> q = new LinkedList<>();;
-    public static Queue<int[]> water = new LinkedList<>();;
-    public static int r, c;
-    public static char[][] map;
-    public static int[][] count;
-    static int answer=Integer.MAX_VALUE;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+    static Deque<int[]> waterQueue = new ArrayDeque<>();
+    static Deque<int[]> moveQueue = new ArrayDeque<>();
+    static int R;
+    static int C;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
 
-        map = new char[r][c];
-        count = new int[r][c];
+        char[][] board = new char[R][C];
+        int[][] times = new int[R][C];
 
-        for (int i = 0; i < r; i++) {
-            String s = br.readLine();
-            for (int j = 0; j < c; j++) {
-                map[i][j] = s.charAt(j);
-                if (map[i][j]=='S') {
-                    q.add(new int[]{i, j,0});
-                }else if(map[i][j]=='*'){
-                    water.add(new int[]{i,j});
+        for (int i = 0; i < R; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < C; j++) {
+                board[i][j] = line.charAt(j);
+                if (board[i][j] == 'S') {
+                    moveQueue.offer(new int[]{i, j});
+                } else if (board[i][j] == '*') {
+                    waterQueue.offer(new int[]{i, j});
                 }
             }
         }
 
-
-        bfs();
-        System.out.println(answer==Integer.MAX_VALUE?"KAKTUS":answer);
-
+        int answer = bfs(board, times);
+        if (answer == -1) {
+            System.out.println("KAKTUS");
+        } else {
+            System.out.println(answer);
+        }
 
     }
 
-    public static void bfs() {
+    static int bfs(char[][] board, int[][] times) {
+        while (!moveQueue.isEmpty()) {
+            int waterSize = waterQueue.size();
+            for (int w = 0; w < waterSize; w++) { // 물 퍼짐
+                int[] cur = waterQueue.poll();
+                for (int i = 0; i < 4; i++) {
+                    int nx = cur[0] + dx[i];
+                    int ny = cur[1] + dy[i];
 
-        while (!q.isEmpty()) {
-            // 물 퍼뜨림
-            int len = water.size();
-            for(int i=0;i<len;i++){
-                int[] t = water.poll();
-                int x = t[0];
-                int y = t[1];
-                for (int k = 0; k < 4; k++) {
-                    int nx = x + dx[k];
-                    int ny = y + dy[k];
-                    if (nx >= 0 && nx < r && ny >= 0 && ny < c && map[nx][ny]=='.') {
-                        map[nx][ny] = '*';
-                        water.add(new int[]{nx,ny});
+                    if (nx >= 0 && nx < R && ny >= 0 && ny < C) {
+                        if (board[nx][ny] == '.') {
+                            board[nx][ny] = '*';
+                            waterQueue.offer(new int[]{nx, ny});
+                        }
                     }
                 }
             }
 
-            // 고슴도치 이동
-            len = q.size();
-            for(int i=0;i<len;i++){
-                int[] t = q.poll();
-                int x = t[0];
-                int y = t[1];
-                int time = t[2];
-                for (int k = 0; k < 4; k++) {
-                    int nx = x + dx[k];
-                    int ny = y + dy[k];
-                    if (nx >= 0 && nx < r && ny >= 0 && ny < c) {
-                        if(map[nx][ny]=='D'){
-                            answer=Math.min(answer,time+1);
-                            return;
-                        }else if(map[nx][ny]=='.'){
-                            map[nx][ny] = 'S';
-                            q.add(new int[]{nx,ny,time+1});
+            int moveSize = moveQueue.size();
+            for (int m = 0; m < moveSize; m++) { // 비버 이동
+                int[] cur = moveQueue.poll();
+                int x = cur[0];
+                int y = cur[1];
+                for (int i = 0; i < 4; i++) {
+                    int nx = x + dx[i];
+                    int ny = y + dy[i];
+
+                    if (nx >= 0 && nx < R && ny >= 0 && ny < C) {
+                        if (board[nx][ny] == 'D') {
+                            return times[cur[0]][cur[1]] + 1;
+                        } else if (board[nx][ny] == '.') {
+                            times[nx][ny] = times[x][y] + 1;
+                            board[nx][ny] = 'S';
+                            moveQueue.offer(new int[]{nx, ny});
                         }
                     }
                 }
             }
         }
+        return -1;
     }
 }
