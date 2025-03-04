6455166507c4205a090498303aa72764ea7a752a
kgy1008 <gayeon1008@g.skku.edu>
2025-03-04 15:22:24 +0900

    [Gold IV] Title: 단어 수학, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -1,35 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		
-		Map<Character, Integer> map = new HashMap<>();
-		List<String> list = new ArrayList<>();
-		
-		for (int i=0; i<n; i++) {
-			String s = br.readLine();
-			list.add(s);
-			int len = s.length();
-			for (int j=0; j<len; j++) {
-				char c = s.charAt(j);
-				int weight = (int) Math.pow(10, len-j-1);
-				map.put(c, map.getOrDefault(c, 0) + weight);
-			}
-		}
-		
-		List<Integer> weights = new ArrayList<>(map.values());
-		weights.sort(Collections.reverseOrder());
-		
-		int num = 9, answer = 0;
-		for (int weight : weights) {
-			answer += weight * num--;
-		}
-		System.out.print(answer);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            int len = s.length() - 1;
+
+            for (int j = 0; j < s.length(); j++) {
+                char c = s.charAt(j);
+                map.put(c, (int) (map.getOrDefault(c, 0) + Math.pow(10, len--)));
+            }
+        }
+
+        List<Integer> list = new ArrayList<>(map.values());
+        list.sort(Collections.reverseOrder());
+
+        int num = 9;
+        int answer = 0;
+
+        for (int value : list) {
+            answer += value * num;
+            num--;
+        }
+
+        System.out.println(answer);
+    }
 }
