6ee8d58307a30edb59eb88113720e47035d93b2c
kgy1008 <gayeon1008@g.skku.edu>
2025-03-04 15:42:22 +0900

    [Gold IV] Title: 부분합, Time: 256 ms, Memory: 23720 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,39 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int answer = Integer.MAX_VALUE;
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int s = Integer.parseInt(st.nextToken());
-		
-		int[] nums = new int[n];
-		st = new StringTokenizer(br.readLine());
-		for (int i=0; i<n; i++) {
-			nums[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		solve(nums, s);
-		int result = answer == Integer.MAX_VALUE ? 0 : answer;
-		System.out.println(result);
-	}
-	
-	static void solve(int[] nums, int target) {
-		int low = 0, end = 0, sum = 0;
-		
-		while (end < nums.length) {
-			sum += nums[end++];
-			while (sum >= target) {
-				answer = Math.min(answer, end - low);
-				sum -= nums[low++];
-			}
-		}
-		
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+
+        int[] num = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int right = 0;
+        int sum = 0;
+        int answer = 100001;
+        for (int left = 0; left < n; left++) {
+            while (right < n && sum < s) {
+                sum += num[right++];
+            }
+
+            if (sum >= s) {
+                answer = Math.min(answer, right - left);
+            } else {
+                if (right == n) {
+                    break;
+                }
+            }
+            sum -= num[left];
+        }
+
+        System.out.println(answer == 100001 ? 0 : answer);
+    }
 }
