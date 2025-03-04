a6666ec75a4b0eadee379a80a988b954473ba2e8
kgy1008 <gayeon1008@g.skku.edu>
2025-03-04 18:38:29 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 304 ms, Memory: 23720 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -3,28 +3,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Main {
+public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
         int[][] board = new int[n][m];
 
         for (int i = 0; i < n; i++) {
-            String s = br.readLine();
+            String line = br.readLine();
             for (int j = 0; j < m; j++) {
-                board[i][j] = s.charAt(j) - '0';
+                board[i][j] = line.charAt(j) - '0';
             }
         }
 
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < m; j++) {
                 if (board[i][j] == 1) {
-                    board[i][j] = Math.min(Math.min(board[i][j - 1], board[i - 1][j]), board[i - 1][j - 1]) + 1;
+                    board[i][j] = Math.min(board[i - 1][j], Math.min(board[i - 1][j - 1], board[i][j - 1])) + 1;
                 }
             }
         }
@@ -32,9 +31,7 @@ class Main {
         int max = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (board[i][j] > max) {
-                    max = board[i][j];
-                }
+                max = Math.max(board[i][j], max);
             }
         }
 
