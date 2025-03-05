312e1b1b36ddd95777785353488f36e26d272a1c
kgy1008 <gayeon1008@g.skku.edu>
2025-03-05 12:05:02 +0900

    [Gold II] Title: 가운데를 말해요, Time: 968 ms, Memory: 39316 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -1,44 +1,36 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.*;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		PriorityQueue<Integer> minHeap = new PriorityQueue<>(); //최소 힙
-		PriorityQueue<Integer> maxHeap = new PriorityQueue<>((o1,o2) -> Integer.compare(o2, o1)); // 최대 힙
-		
-		int n = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		
-		for (int i=0; i<n; i++) {
-			int num = Integer.parseInt(br.readLine());
-			
-			if (minHeap.size() == maxHeap.size()) {
-				maxHeap.offer(num);
-			} else {
-				minHeap.offer(num);
-			}
-			
-			if (!minHeap.isEmpty() &&  !maxHeap.isEmpty()) {
-				if (maxHeap.peek() > minHeap.peek()) {
-					int temp = maxHeap.poll();
-					maxHeap.offer(minHeap.poll());
-					minHeap.offer(temp);
-				}
-			}
-			sb.append(maxHeap.peek()).append("\n");
-		}
 
-		String answer = sb.toString();
-		bw.write(answer);
-		bw.flush();
-		
-		br.close();
-        bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());  // 왼쪽 위치
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();  // 오른쪽 위치
+
+        for (int i = 0; i < N; i++) {
+            int number = Integer.parseInt(br.readLine());
+
+            if (maxHeap.size() <= minHeap.size()) {
+                maxHeap.add(number);
+            } else {
+                minHeap.add(number);
+            }
+
+            if (!maxHeap.isEmpty() && !minHeap.isEmpty()) {
+                if (maxHeap.peek() > minHeap.peek()) {  // swap
+                    int temp1 = maxHeap.poll();
+                    int temp2 = minHeap.poll();
+                    maxHeap.add(temp2);
+                    minHeap.add(temp1);
+                }
+            }
+            System.out.println(maxHeap.peek());
+        }
+    }
 }
