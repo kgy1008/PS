c2c08f8b09cbe3c7a945e7718ca1d5f7bbe83efd
kgy1008 <gayeon1008@g.skku.edu>
2025-03-05 17:06:45 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,12 +1,14 @@
-with recursive time as (
-    select 0 as n
-    union all
-    select n+1
-    from time
-    where n<23
+WITH RECURSIVE full_hour AS (
+    SELECT 0 AS hour
+    
+    UNION ALL
+    
+    SELECT hour+1
+    FROM full_hour
+    WHERE hour < 23
 )
 
-select t.n as hour, count(a.animal_id)
-from time t left join ANIMAL_OUTS a on hour(a.datetime) = t.n
-group by hour
-order by hour
+SELECT h.hour, COUNT(a.DATETIME) AS COUNT
+FROM full_hour h LEFT JOIN ANIMAL_OUTS a ON h.hour = HOUR(a.DATETIME)
+GROUP BY h.hour
+ORDER BY h.hour
