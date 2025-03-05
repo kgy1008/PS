69c01bfa5e4e61dee7fab76530f89471fd393677
kgy1008 <gayeon1008@g.skku.edu>
2025-03-05 14:22:12 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 1140 ms, Memory: 62332 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
@@ -3,55 +3,63 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
-	static boolean[] isPrime = new boolean[1000000];
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		Arrays.fill(isPrime, true);
-		getPrimeNum();
 
-		while(true) {
-			int n = Integer.parseInt(br.readLine());
-			if (n==0) break;
-			
-			boolean isWrong = true;
-			int start = 3;
-			while (start <= n / 2) {
-				if (isPrime[start]) {
-					int target = n - start;
-					if (isPrime[target]) {
-						bw.write(n + " = " + start + " + " + target);
-						bw.newLine();
-						isWrong = false;
-						break;
-					}
-				}
-				start++;
-			}
-			if (isWrong) {
-				bw.write("Goldbach's conjecture is wrong.");
-                bw.newLine();
-			}
-		}
-		
-		br.close();
+    static boolean[] isPrime = new boolean[1000001];
+    static Set<Integer> primeSet = new TreeSet<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Arrays.fill(isPrime, true);
+        getPrimes();
+
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) {
+                break;
+            }
+
+            String answer = solve(n);
+            bw.write(answer);
+        }
+
+        br.close();
         bw.flush();
         bw.close();
-	}
-	
-	 static void getPrimeNum() {
-			isPrime[0]=isPrime[1]=false; 
-			
-			for(int i=2;i*i<1000000;i++){
-				if(isPrime[i]){
-					for(int j=i*i;j<1000000;j+=i) { 
-						isPrime[j] = false;
-					}
-				}
-			}
-		}
+    }
+
+    private static String solve(int n) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(n).append(" = ");
+        for (int num : primeSet) {
+            if (primeSet.contains(n - num)) {
+                sb.append(num).append(" + ").append(n - num);
+                break;
+            }
+        }
+        sb.append("\n");
+        return sb.toString();
+    }
+
+    private static void getPrimes() {
+        for (int i = 2; i < Math.sqrt(1000001) + 1; i++) {
+            if (isPrime[i]) {
+                for (int j = 2; i * j < 1000001; j++) {
+                    isPrime[i * j] = false;
+                }
+            }
+        }
+
+        for (int i = 2; i < 1000001; i++) {
+            if (isPrime[i]) {
+                primeSet.add(i);
+            }
+        }
+    }
 }
