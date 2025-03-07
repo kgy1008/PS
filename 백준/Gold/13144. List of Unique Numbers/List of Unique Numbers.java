10e0fbb93b27872ce4d9791dc83cc65b57603ab5
kgy1008 <gayeon1008@g.skku.edu>
2025-03-07 19:26:45 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 340 ms, Memory: 35204 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.java
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.java
@@ -6,35 +6,32 @@ import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
         int n = Integer.parseInt(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         int[] nums = new int[n];
-
         for (int i = 0; i < n; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(solve(nums, n));
-    }
+        int start = 0;
+        int end = 0;
+        long answer = 0L;
 
-    public static long solve(int[] arr, int n) {
-        long count = 0;
-        int left = 0;
         Set<Integer> set = new HashSet<>();
-
-        for (int right = 0; right < n; right++) {
-            while (set.contains(arr[right])) {
-                set.remove(arr[left]);
-                left++;
+        while (start < n) {
+            while (end < n && !set.contains(nums[end])) {
+                set.add(nums[end++]);
             }
-
-            set.add(arr[right]);
-            count += (right - left + 1);
+            answer += (end - start);
+            set.remove(nums[start++]);
         }
 
-        return count;
+        System.out.println(answer);
     }
 }
