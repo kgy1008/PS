103a875700c1b947d5aa249ff837537a5a6e6c7c
kgy1008 <gayeon1008@g.skku.edu>
2025-03-07 18:23:37 +0900

    [Gold III] Title: 게임 개발, Time: 256 ms, Memory: 22180 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.java
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -1,91 +1,72 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-	static List<Integer>[] adj;
-	static int[] degree;
-	static int[] weight;
-	static int answer = 0;
-	static int[] time;
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int n = Integer.parseInt(br.readLine()); // 건물의 종류 수
-		 adj = new ArrayList[n+1];
-		 degree = new int[n+1];
-		 weight = new int[n+1];
-		 time = new int[n+1];
-		
-		for (int i=1; i<=n; i++) {
-			adj[i] = new ArrayList<>();
-		}
-		
-		for (int i=1; i<=n; i++) {
-			String[] element = br.readLine().split(" ");
-			for (int j=0; j<element.length; j++) {
-				int target = Integer.parseInt(element[j]);
-				if (j==0) {
-					weight[i] = target; // 건설하는데 걸리는 시간 저장
-				} else {
-					if (target != -1) {
-						adj[target].add(i);
-						degree[i]++;  // 차수 증가
-					}
-				}
-			}
-		}
-		
-		solve(n);
 
-		StringBuilder sb = new StringBuilder();
-		for (int t=1; t<=n; t++) {
-			sb.append(time[t]).append("\n");
-		}
-		
-		bw.write(sb.toString());
-		bw.flush();
-		
-		br.close();
-		bw.close();
-	}
-	
-	static void solve(int n) {
-		Deque<Integer> todo = new ArrayDeque<>();
-		
-		for (int i=1; i<=n; i++) {
-			if (degree[i] == 0) {  // 가장 우선순위로 작업해야 하는 건물 큐에 집어넣기
-				todo.offer(i);
-			}
-		}
-		
-		while(!todo.isEmpty()) {
-			List<Integer> list = new ArrayList<>(todo);
-			
-			int min = list.stream().mapToInt(i -> weight[i]).min().getAsInt();
-			
-			for (int i : list) {
-				if (weight[i] == min) {
-					todo.remove(i);
-					time[i] = answer + min;
-					
-					for (int next : adj[i]) {
-						degree[next]--;
-						if (degree[next] == 0) {
-							todo.offer(next);
-						}
-					}
-				} 
-				else {
-					weight[i] -= min;
-				}
-			}
-			answer += min;
-		}
-	}
+    static int[] time;
+    static int[] indegree;
+    static List<Integer>[] adjList;
+    static int[] answer;
+    static ArrayDeque<Integer> queue = new ArrayDeque<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine()); // 건물의 종류
+        time = new int[N + 1];
+        answer = new int[N + 1];
+        indegree = new int[N + 1];
+        adjList = new ArrayList[N + 1];
+        for (int i = 0; i <= N; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(st.nextToken()); // 짓는데 걸리는 시간
+            time[i] = t;
+
+            while (st.hasMoreTokens()) {
+                int a = Integer.parseInt(st.nextToken());  // 선행되어야 하는 건물 번호
+                if (a == -1) {
+                    break;
+                }
+                indegree[i]++;
+                adjList[a].add(i);
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i); // 진입 차수가 0인 건물들부터 추가
+            }
+        }
+
+        solve();
+        for (int i = 1; i <= N; i++) {
+            System.out.println(answer[i]);
+        }
+    }
+
+    private static void solve() {
+        while (!queue.isEmpty()) {
+            int prev = queue.poll();
+            answer[prev] += time[prev];
+
+            for (int next : adjList[prev]) {
+                indegree[next]--;
+                answer[next] = Math.max(answer[prev], answer[next]);
+
+                if (indegree[next] == 0) {
+                    queue.offer(next);
+                }
+            }
+        }
+    }
 }
