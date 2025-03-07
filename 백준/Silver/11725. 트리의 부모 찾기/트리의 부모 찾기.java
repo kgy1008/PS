91219b66f48623cf29c2ef7727e784f42ad348b2
kgy1008 <gayeon1008@g.skku.edu>
2025-03-07 23:10:56 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1160 ms, Memory: 80744 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,48 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+    static List<Integer>[] adjList;
+    static int[] parent;
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        int n = Integer.parseInt(br.readLine());
-        int[] parent = new int[n + 1];
-        ArrayList<Integer>[] adj = new ArrayList[n + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        parent = new int[n + 1];
+        visited = new boolean[n + 1];
+        adjList = new List[n + 1];
         for (int i = 1; i <= n; i++) {
-            adj[i] = new ArrayList<>();
+            adjList[i] = new LinkedList<>();
         }
-        boolean[] visited = new boolean[n + 1];
-        for (int i = 1; i < n; i++) {
+
+        for (int i = 0; i < n - 1; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            adj[a].add(b);
-            adj[b].add(a);
+            adjList[a].add(b);
+            adjList[b].add(a);
         }
 
-        Deque<Integer> queue = new ArrayDeque<>();
-        queue.add(1);
-        visited[1] = true;
-        while (!queue.isEmpty()) {
-            int cur = queue.poll();
-            for (int next : adj[cur]) {
-                if (!visited[next]) {
-                    visited[next] = true;
-                    queue.add(next);
-                    parent[next] = cur;
-                }
-            }
-        }
+        dfs(1);
 
         for (int i = 2; i <= n; i++) {
             System.out.println(parent[i]);
+        }
+    }
+
+    private static void dfs(int node) {
+        visited[node] = true;
+
+        for (int next : adjList[node]) {
+            if (!visited[next]) {
+                parent[next] = node;
+                visited[next] = true;
+                dfs(next);
+            }
         }
     }
 }
