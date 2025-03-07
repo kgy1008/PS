85fecacffe8d701e006335ce3f6bd5b69e647e7f
kgy1008 <gayeon1008@g.skku.edu>
2025-03-07 19:06:23 +0900

    [Gold III] Title: 줄 세우기, Time: 420 ms, Memory: 45636 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,68 +1,64 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-	static List<Integer>[] list;
-	static int[] degree;
-	static final StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());  // 전체 학생 수
-		int m = Integer.parseInt(st.nextToken());  // 키를 비교한 횟수
-		
-		 list = new ArrayList[n+1];
-		 degree = new int[n+1];
-		
-		for (int i=1; i<=n; i++) {
-			list[i] = new ArrayList<>();
-		}
-		
-		while (m --> 0) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			list[a].add(b);
-			degree[b]++;
-		}
-		
-		solve(n);
+    static int[] indegree;
+    static List<Integer>[] adjList;
+    static ArrayDeque<Integer> queue = new ArrayDeque<>();
 
-		bw.write(sb.toString());
-		bw.flush();
-		
-		br.close();
-		bw.close();
-	}
-	
-	static void solve(int n) {
-		Deque<Integer> queue = new ArrayDeque<>();
-		
-		for (int i=1; i<=n; i++) {
-			if (degree[i] == 0) {
-				queue.offer(i);
-			}
-		}
-		
-		while (!queue.isEmpty()) {
-			int cur = queue.poll();
-			sb.append(cur).append(" ");
-			
-			for (int next : list[cur]) {
-				degree[next]--;
-				
-				if (degree[next] == 0) {
-					queue.offer(next);
-				}
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()); // 사람 수
+        int M = Integer.parseInt(st.nextToken()); // 키를 비교한 횟수
+
+        indegree = new int[N + 1];
+        adjList = new ArrayList[N + 1];
+        for (int i = 0; i <= N; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            adjList[a].add(b);
+            indegree[b]++;
+        }
+
+        String answer = topologicalSort(N);
+        System.out.println(answer);
+    }
+
+    private static String topologicalSort(int n) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            if (indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            sb.append(cur).append(" ");
+
+            for (int next : adjList[cur]) {
+                indegree[next]--;
+
+                if (indegree[next] == 0) {
+                    queue.offer(next);
+                }
+            }
+        }
+
+        return sb.toString();
+    }
 }
