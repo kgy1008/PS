9cbdff6ee5e9de9d0f39e5215bb4e69f8c5eb336
kgy1008 <gayeon1008@g.skku.edu>
2025-03-10 19:15:04 +0900

    [level 2] Title: 큰 수 만들기, Time: 66.22 ms, Memory: 99.1 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,35 +1,30 @@
 import java.util.*;
+
 class Solution {
     public String solution(String number, int k) {
-        // 그리디 알고리즘
-        // 각 자리에서 최고로 높은 수가 나오는 경우를 생각하기
+        Stack<Integer> stack = new Stack<>();
         
-        String answer = "";
-        StringBuilder answerBuilder = new StringBuilder();
-
-        
-        char[] array = number.toCharArray();
-        
-        int len = array.length - k;
-        
-        // 문자 비교를 시작하는 인덱스를 나타내는 start 변수 
-        int start = 0;
-
-        for(int i =0; i<len; i++){
-            char max = '0';
-            for(int j = start; j <= i + k; j++){
-                // 가장 큰수를 골라서 그 다음 인덱스를 시작 인덱스로 설정하기 
-                if(array[j] > max){
-                    max = array[j];
-                    start=j+1;
-                }
-            }
-            // 가장 큰 문자를 String에 넣어주기
-            answerBuilder.append(max);
+        int len = number.length();
+        for (int i=0; i<len; i++) {
+            int num = number.charAt(i) -'0';
+            while (k>0 && !stack.isEmpty() && stack.peek() < num) {
+                stack.pop();
+                k--;
+            } 
+            stack.push(num);
         }
         
-        // k개의 수를 제거할 때 얻을 수 있는 가장 큰 숫자를 구하려 한다 
-        return answerBuilder.toString();
+        while (k > 0) {
+            stack.pop();
+            k--;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        
+        String answer = sb.reverse().toString();
+        return answer;
     }
-    
 }
