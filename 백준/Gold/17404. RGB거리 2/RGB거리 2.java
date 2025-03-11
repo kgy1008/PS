91cc6964dd55b3c1041161696c03a88dcb453c09
kgy1008 <gayeon1008@g.skku.edu>
2025-03-11 19:59:50 +0900

    [Gold IV] Title: RGB거리 2, Time: 116 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.java
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,52 +10,75 @@ public class Main {
         StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-        int[][] cost = new int[n][3];
-        int[][] dp = new int[n][3];
+        int[][] color = new int[n][3];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            cost[i][0] = Integer.parseInt(st.nextToken());  // R
-            cost[i][1] = Integer.parseInt(st.nextToken());  // G
-            cost[i][2] = Integer.parseInt(st.nextToken());  // B
+            int r = Integer.parseInt(st.nextToken());
+            int g = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            color[i][0] = r;
+            color[i][1] = g;
+            color[i][2] = b;
         }
 
-        int answer = Integer.MAX_VALUE;
+        int answer = 1_000_001;
 
-        // 첫번째 집 -> R
-        dp[0][0] = cost[0][0];
-        dp[0][1] = 1001;  // G 불가능
-        dp[0][2] = 1001;  // B 불가능
+        // 첫번째 집 빨간색 선택
+        int[] dp = new int[3];
+        Arrays.fill(dp, 1_000_001);
+        dp[0] = color[0][0];
         for (int i = 1; i < n; i++) {
-            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
-            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
-        }
-        answer = Math.min(answer, Math.min(dp[n - 1][1], dp[n - 1][2]));  // 마지막 집은 초록 또는 파랑
+            int red = Math.min(dp[1], dp[2]) + color[i][0];
+            int green = Math.min(dp[0], dp[2]) + color[i][1];
+            int blue = Math.min(dp[0], dp[1]) + color[i][2];
 
-        // 첫번째 집 -> G
-        dp[0][0] = 1001;  // R 불가능
-        dp[0][1] = cost[0][1];
-        dp[0][2] = 1001;  // B 불가능
+            dp[0] = red;
+            dp[1] = green;
+            dp[2] = blue;
+
+            if (i == n - 1) { // 마지막 집
+                answer = Math.min(Math.min(answer, dp[1]), dp[2]);
+            }
+        }
+
+        // 첫번째 집 초록색 선택
+        dp = new int[3];
+        Arrays.fill(dp, 1_000_001);
+        dp[1] = color[0][1];
         for (int i = 1; i < n; i++) {
-            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
-            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
-        }
-        answer = Math.min(answer, Math.min(dp[n - 1][0], dp[n - 1][2]));  // 마지막 집은 빨강 또는 파랑
+            int red = Math.min(dp[1], dp[2]) + color[i][0];
+            int green = Math.min(dp[0], dp[2]) + color[i][1];
+            int blue = Math.min(dp[0], dp[1]) + color[i][2];
 
-        // 첫번째 집 -> B
-        dp[0][0] = 1001;  // R 불가능
-        dp[0][1] = 1001;  // G 불가능
-        dp[0][2] = cost[0][2];
+            dp[0] = red;
+            dp[1] = green;
+            dp[2] = blue;
+
+            if (i == n - 1) { // 마지막 집
+                answer = Math.min(Math.min(answer, dp[0]), dp[2]);
+            }
+        }
+
+        // 첫번째 집 파란색 선택
+        dp = new int[3];
+        Arrays.fill(dp, 1_000_001);
+        dp[2] = color[0][2];
         for (int i = 1; i < n; i++) {
-            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
-            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
-        }
-        answer = Math.min(answer, Math.min(dp[n - 1][0], dp[n - 1][1]));  // 마지막 집은 빨강 또는 초록
+            int red = Math.min(dp[1], dp[2]) + color[i][0];
+            int green = Math.min(dp[0], dp[2]) + color[i][1];
+            int blue = Math.min(dp[0], dp[1]) + color[i][2];
 
-        // 결과 출력
+            dp[0] = red;
+            dp[1] = green;
+            dp[2] = blue;
+
+            if (i == n - 1) { // 마지막 집
+                answer = Math.min(Math.min(answer, dp[0]), dp[1]);
+            }
+        }
+
         System.out.println(answer);
     }
 }
