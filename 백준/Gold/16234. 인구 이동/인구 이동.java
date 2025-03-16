395a7ed62e68dd0f3efb3d6841b54d80e6177aba
kgy1008 <gayeon1008@g.skku.edu>
2025-03-16 13:56:50 +0900

    [Gold IV] Title: 인구 이동, Time: 1208 ms, Memory: 305204 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -1,94 +1,117 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
     static int[][] board;
-    static boolean[][] visited;
+    static int[] dx = new int[]{0, 0, 1, -1};
+    static int[] dy = new int[]{1, -1, 0, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int l = Integer.parseInt(st.nextToken());
-        int r = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
 
-        board = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
+        board = new int[N][N];
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < N; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int ans = 0;
+        int answer = 0;
         while (true) {
-            visited = new boolean[n][n];
+            boolean[][] visited = new boolean[N][N];
             boolean moved = false;
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
                     if (!visited[i][j]) {
-                        if (bfs(l, r, i, j)) {
+                        ArrayDeque<Point> queue = new ArrayDeque<>();
+                        Set<Point> set = new HashSet<>();
+
+                        queue.add(new Point(i, j));
+                        visited[i][j] = true;
+                        set.add(new Point(i, j));
+
+                        int sum = 0;
+                        sum += board[i][j];
+
+                        while (!queue.isEmpty()) {
+                            Point cur = queue.poll();
+                            int x = cur.x;
+                            int y = cur.y;
+
+                            for (int d = 0; d < 4; d++) {
+                                int nx = x + dx[d];
+                                int ny = y + dy[d];
+
+                                if (nx >= 0 && nx < N && ny >= 0 && ny < N && !visited[nx][ny]) {
+                                    int diff = Math.abs(board[nx][ny] - board[x][y]);
+
+                                    if (diff >= L && diff <= R) {
+                                        queue.add(new Point(nx, ny));
+                                        visited[nx][ny] = true;
+                                        set.add(new Point(nx, ny));
+                                        sum += board[nx][ny];
+                                    }
+                                }
+                            }
+                        }
+
+                        // 연합이 2개 이상인 경우 인구 이동 처리
+                        if (set.size() > 1) {
                             moved = true;
+                            int average = sum / set.size();
+                            for (Point p : set) {
+                                board[p.x][p.y] = average;
+                            }
                         }
                     }
                 }
             }
-            if (!moved) break;
-            ans++;
+
+            if (!moved) {
+                break;  // 더 이상 인구 이동이 없으면 종료
+            }
+            answer++;
         }
-        System.out.println(ans);
+        System.out.println(answer);
     }
 
-    private static boolean bfs(int l, int r, int startX, int startY) {
-        ArrayDeque<int[]> queue = new ArrayDeque<>();
-        Set<int[]> union = new HashSet<>();
+    static class Point {
+        int x;
+        int y;
 
-        queue.add(new int[]{startX, startY});
-        visited[startX][startY] = true;
-        union.add(new int[]{startX, startY});
-
-        int[] dx = new int[]{1, -1, 0, 0};
-        int[] dy = new int[]{0, 0, 1, -1};
-
-        int totalPeople = board[startX][startY];
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int x = cur[0];
-            int y = cur[1];
-
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if (nx < 0 || nx >= board.length || ny < 0 || ny >= board.length) {
-                    continue;
-                }
-
-                if (!visited[nx][ny]) {
-                    int diff = Math.abs(board[x][y] - board[nx][ny]);
-                    if (diff >= l && diff <= r) {
-                        queue.offer(new int[]{nx, ny});
-                        visited[nx][ny] = true;
-                        union.add(new int[]{nx, ny});
-                        totalPeople += board[nx][ny];
-                    }
-                }
-            }
+        public Point(final int x, final int y) {
+            this.x = x;
+            this.y = y;
         }
 
-        if (union.size() > 1) {
-            int newPopulation = totalPeople / union.size();
-            for (int[] coordinate : union) {
-                board[coordinate[0]][coordinate[1]] = newPopulation;
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) {
+                return true;
             }
-            return true;
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Point point = (Point) o;
+            return x == point.x && y == point.y;
         }
 
-        return false;
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
     }
 }
