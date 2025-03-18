14551d929863c0dd4b1b39136fa8891d3fcfbe3a
kgy1008 <gayeon1008@g.skku.edu>
2025-03-18 13:45:50 +0900

    [Silver IV] Title: 대칭 차집합, Time: 676 ms, Memory: 83692 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Set<Integer> A = new HashSet<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Set<Integer> B = new HashSet<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            B.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int answer = 0;
+        for (int a : A) {
+            if (!B.contains(a)) {
+                answer++;
+            }
+        }
+
+        for (int b : B) {
+            if (!A.contains(b)) {
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
