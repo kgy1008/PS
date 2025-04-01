a30770137e5cb78d0a0c3e5c9ef9786e58ccc6a2
kgy1008 <gayeon1008@g.skku.edu>
2025-04-01 20:35:40 +0900

    [level 2] Title: 피로도, Time: 3.26 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,21 +1,27 @@
 class Solution {
-    int answer = 0;
-    boolean[] visited;
+    static int n;
+    static int[][] DUNGEONS;
+    static boolean[] visited;
+    static int answer = 0;
     
     public int solution(int k, int[][] dungeons) {
-        visited = new boolean[dungeons.length];
-        backtrack(k, dungeons, 0);
+        n = dungeons.length;
+        visited = new boolean[n];
+        DUNGEONS = dungeons;
+        
+        solve(k,0);
         return answer;
     }
     
-    private void backtrack(int k, int[][]dungeons, int cnt) {
-        for (int i=0; i<dungeons.length; i++) {
-            if (k>= dungeons[i][0] && !visited[i]) {
+    private static void solve(int k, int count) {
+        for (int i=0; i<n; i++) {
+            if (!visited[i] && k >= DUNGEONS[i][0]) {
                 visited[i] = true;
-                backtrack(k-dungeons[i][1], dungeons, cnt+1);
-                answer = Math.max(answer,cnt+1);
+                solve(k-DUNGEONS[i][1], count+1);
                 visited[i] = false;
             }
         }
+        
+        answer = Math.max(answer, count);
     }
 }
