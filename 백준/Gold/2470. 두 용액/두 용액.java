669cfe7cc049d7a4291cef289541c6d8b5c9dcc1
kgy1008 <gayeon1008@g.skku.edu>
2025-04-01 16:59:11 +0900

    [Gold V] Title: 두 용액, Time: 344 ms, Memory: 28472 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] nums = new int[N];
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        br.close();
+        Arrays.sort(nums);
+
+        int start = 0;
+        int end = N - 1;
+        int min = 2_000_000_001;
+
+        int left = start;
+        int right = end;
+
+        while (start < end) {
+            int mix = nums[start] + nums[end];
+            if (mix == 0) {
+                System.out.println(nums[start] + " " + nums[end]);
+                return;
+            }
+
+            if (Math.min(Math.abs(min), Math.abs(mix)) == Math.abs(mix)) {
+                min = Math.min(Math.abs(min), Math.abs(mix));
+                left = start;
+                right = end;
+            }
+
+            if (mix < 0) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+
+        System.out.println(nums[left] + " " + nums[right]);
+    }
+}
