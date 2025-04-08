9ef3b1ad35bd64dd087ffe343896dfb43a004d14
kgy1008 <gayeon1008@g.skku.edu>
2025-04-08 16:20:32 +0900

    [Silver II] Title: 나무 자르기, Time: 844 ms, Memory: 119448 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,50 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		
-		int[] tree = new int[n];
-		st = new StringTokenizer(br.readLine());
-		for (int i=0; i<n; i++) {
-			tree[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(tree);
-		int right = tree[n-1];
-		int left = 0;
-		int ans = 0;
-		
-		while (left <= right) {
-			int mid = (right + left) / 2;
-			long get = 0L;
-			for (int t : tree) {
-				if (t > mid) {
-					get += (t-mid);
-				}
-			}
-			
-			if (get < m) {
-				right = mid - 1;
-			} else {
-				if (get == m) {
-					ans = mid;
-					break;
-				}
-				ans = mid;
-				left = mid + 1;
-			}
-		}
-		
-		System.out.print(ans);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	}
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] tree = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            tree[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(tree);
+
+        int low = 0;
+        int high = tree[N - 1];
+        int answer = 0;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            long sum = 0L;
+            for (int t : tree) {
+                if (t > mid) {
+                    sum += (t - mid);
+                }
+            }
+
+            if (sum >= M) {
+                answer = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        System.out.println(answer);
+    }
 }
