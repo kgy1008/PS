a098737a6d5e00f6b0b8d5fdeed2b8f6bb3a6aee
kgy1008 <gayeon1008@g.skku.edu>
2025-04-18 14:46:59 +0900

    [Gold IV] Title: 문자열 폭발, Time: 484 ms, Memory: 61932 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -1,41 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Main {
-    private static int[] num;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String target = br.readLine();
         String bomb = br.readLine();
 
-        StringBuilder sb = new StringBuilder();
         int size = bomb.length();
 
-        for (int i = 0; i < target.length(); i++) {
-            sb.append(target.charAt(i));
+        Stack<Character> stack = new Stack<>();
 
-            if (sb.length() >= size) {
-                boolean match = true;
+        for (int i = 0; i < target.length(); i++) {
+            char c = target.charAt(i);
+            stack.push(c);
+
+            int s = stack.size();
+            boolean flag = true;
+
+            if (s >= size) {
                 for (int j = 0; j < size; j++) {
-                    if (sb.charAt(sb.length() - size + j) != bomb.charAt(j)) {
-                        match = false;
+                    if (stack.get(s - j - 1) != bomb.charAt(size - j - 1)) {
+                        flag = false;
                         break;
                     }
                 }
-                if (match) {
-                    sb.delete(sb.length() - size, sb.length());
+            }
+
+            if (s >= size && flag) { // 모두 일치한다면
+                for (int j = 0; j < size; j++) {
+                    stack.pop();
                 }
             }
         }
 
-        if (sb.isEmpty()) {
-            System.out.println("FRULA");
-        } else {
-            System.out.println(sb);
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
         }
+        sb.reverse();
+        String answer = sb.toString();
+        System.out.println(answer.isEmpty() ? "FRULA" : answer);
     }
-
 }
