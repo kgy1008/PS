6a461ee335a830318dac753913f18f19daafff4f
kgy1008 <gayeon1008@g.skku.edu>
2025-04-18 15:43:31 +0900

    [level 3] Title: 기지국 설치, Time: 3.39 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,22 +1,23 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n, int[] stations, int w) {
-        double maxDistance = 1.0 + 2*w;
-        int start = 1, end = 1;
         int answer = 0;
+        int start = 1;  // 처음 아파트 번호
         
-        for (int s : stations) {
-            end = s - w;
-            if (start < end) {
-                int uncovered = end - start; 
-                answer += (int) Math.ceil(uncovered / maxDistance); 
+        for (int station : stations) {
+            int left = station - w;  // 현재 기지국 커버 시작
+            if (start < left) {
+                int gap = left - start;
+                answer += Math.ceil((double)gap / (2 * w + 1));
             }
-            start = s + w + 1;
+            start = station + w + 1;  // 다음 커버되지 않은 구간의 시작
         }
         
-        
+        // 마지막 기지국 이후 구간
         if (start <= n) {
-            int uncovered = n - start + 1;
-            answer += (int) Math.ceil(uncovered / maxDistance);
+            int gap = n - start + 1;
+            answer += Math.ceil((double)gap / (2 * w + 1));
         }
         
         return answer;
