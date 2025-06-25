e25295b1ea571702ff9eaac4fbb737ac1884faf6
Jian Kim <gayeon1008@g.skku.edu>
2025-06-26 00:10:32 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 36.57 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,33 +1,58 @@
 import java.util.*;
-import java.util.regex.*;
 
 class Solution {
-    private static final Pattern pattern = Pattern.compile("([^\\d]+)(\\d+)(.*)");
-    
     public String[] solution(String[] files) {
-        Arrays.sort(files, (o1,o2) -> {
-            String[] arr1 = parse(o1);
-            String[] arr2 = parse(o2);
-            int result = arr1[0].toUpperCase().compareTo(arr2[0].toUpperCase());
-            if (result == 0) {
-                int a = Integer.parseInt(arr1[1]);
-                int b = Integer.parseInt(arr2[1]);
-                return Integer.compare(a,b);
+        List<Word> list = new ArrayList<>();
+        
+        for (String file : files) {
+            String head = "";
+            String number = "";
+            int i = 0;
+            
+            // HEAD 추출
+            while (i < file.length() && !Character.isDigit(file.charAt(i))) {
+                head += file.charAt(i);
+                i++;
             }
-            return result;
-        });
-        return files; 
-    }
-    
-    public static String[] parse(String filename) {
-        Matcher matcher = pattern.matcher(filename);
-        if (matcher.matches()) {
-            String head = matcher.group(1);
-            String number = matcher.group(2);
-            String tail = matcher.group(3);
-            return new String[]{head, number, tail};
+            
+            // NUMBER 추출 (최대 5자리)
+            while (i < file.length() && Character.isDigit(file.charAt(i)) && number.length() < 5) {
+                number += file.charAt(i);
+                i++;
+            }
+ 
+            list.add(new Word(file, head, number));
         }
+        
+        // 정렬
+        list.sort((o1, o2) -> {
+            int headCompare = o1.head.toLowerCase().compareTo(o2.head.toLowerCase());
+            if (headCompare == 0) {
+                int num1 = Integer.parseInt(o1.number);
+                int num2 = Integer.parseInt(o2.number);
+                return Integer.compare(num1, num2); 
+            }
+            
+            return headCompare;
+        });
+        
+        // 결과
+        String[] result = new String[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i).original;
+        }
+        return result;
+    }
 
-        return new String[]{filename, "", ""};
+    private class Word {
+        String original;
+        String head;
+        String number;
+
+        Word(String original, String head, String number) {
+            this.original = original;
+            this.head = head;
+            this.number = number;
+        }
     }
 }
