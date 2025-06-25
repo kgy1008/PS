1dc59a1a5f201803d9f18a5e1f2ed830e31cfbba
Jian Kim <gayeon1008@g.skku.edu>
2025-06-25 18:41:11 +0900

    [level 2] Title: H-Index, Time: 327.98 ms, Memory: 212 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,18 +2,29 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        Arrays.sort(citations);
+        Map<Integer, Integer> map = new HashMap<>(); // 인용 횟수 - 인용 횟수 이상 인용된 논문의 개수
+        
+        for (int i=0; i<= 10000; i++) {
+            map.put(i,0);
+        }
+        
+        for (int c : citations) {
+            for (int key : map.keySet()) {
+                if (key <= c) {
+                    map.put(key, map.get(key) + 1);
+                }
+            }
+        }
+        
         int answer = 0;
         
-        for(int i = 0; i < citations.length; i++) {
-			int h = citations.length - i; 
-			
-			if(citations[i] >= h) {
-				answer = h;
-				break;
-			}
-		}
-        
-        return answer; 
+        for (int h=0; h<10000; h++) {
+            int v = map.get(h); // h번 이상 인용된 논문의 개수
+            
+            if (v >= h && citations.length - v <= h) { // h번 이상 인용된 논문이 h편 이상일 때
+                answer = Math.max(answer, h);
+            }
+        }
+        return answer;
     }
 }
