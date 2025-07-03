1e240bb216c4b28391e173753b411220f15fe402
Jian Kim <gayeon1008@g.skku.edu>
2025-07-03 15:16:52 +0900

    [Silver II] Title: 서버실, Time: 528 ms, Memory: 94152 KB -BaekjoonHub

--- 백준/Silver/17245. 서버실/서버실.java
+++ 백준/Silver/17245. 서버실/서버실.java
@@ -1,47 +1,56 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    private static int[][] computers;
+    private static long totalComputers = 0L;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[][] grid = new int[N][N];
-        long total = 0;
+        int n = Integer.parseInt(st.nextToken());
+
+        computers = new int[n][n];
+
         int max = 0;
-
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                grid[i][j] = Integer.parseInt(st.nextToken());
-                total += grid[i][j];
-                max = Math.max(max, grid[i][j]);
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                computers[i][j] = Integer.parseInt(st.nextToken());
+                totalComputers += computers[i][j];
+                if (computers[i][j] > max) {
+                    max = computers[i][j];
+                }
             }
         }
 
-        long left = 0;
-        long right = max;
-        long answer = 0;
+        int answer = solve(0, max, n);
+        System.out.println(answer);
+    }
+
+    private static int solve(int left, int right, int n) {
+        int answer = 0;
 
         while (left <= right) {
-            long mid = (left + right) / 2;
+            int mid = (left + right) / 2;
+            long count = 0L;
 
-            long installed = 0;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    installed += Math.min(mid, grid[i][j]);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    count += Math.min(computers[i][j], mid);
                 }
             }
 
-            if (installed * 2 >= total) {
-                answer = mid;
+            if (count >= (totalComputers + 1) / 2) { // 절반 이상 켜짐
+                answer = mid; // 현재 mid가 가능한 최대값
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-
-        System.out.println(answer);
+        return answer;
     }
 }
