518edd23972850823718737ed63eb4153b0aa4b8
Jian Kim <gayeon1008@g.skku.edu>
2025-07-19 17:30:20 +0900

    [level 1] Title: 실패율, Time: 49.50 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -2,29 +2,39 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
-        int[] arr = new int[N+2];
-        int person = stages.length;
-        
-        for (int s : stages) {
-            arr[s]++;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Map<Double, List<Integer>> failure = new TreeMap<>((o1, o2) -> Double.compare(o2, o1)); // 내림차순 정렬
+        int people = stages.length;
+
+        // 각 스테이지에 도달한 사람 수 기록
+        for (int stage : stages) {
+            map.put(stage, map.getOrDefault(stage, 0) + 1);
         }
-        
-        
-        Map<Integer, Double> detail = new HashMap<>();
-        for (int stage=1; stage<=N; stage++) {
-            if (person == 0) {
-                detail.put(stage, 0.0);
-                continue;
+
+        // 실패율 계산
+        for (int i = 1; i <= N; i++) {
+            int value = map.getOrDefault(i, 0);
+            double fail = 0.0;
+            if (people > 0) {
+                fail = (double) value / people;
             }
-            int arrived = arr[stage];
-            double failure = (double) arrived / person;
-            detail.put(stage, failure);
-            person -= arrived;
+            List<Integer> list = failure.getOrDefault(fail, new ArrayList<>());
+            list.add(i); 
+            failure.put(fail, list); 
+            people -= value;
         }
-        
-        return detail.entrySet().stream()
-            .sorted((o1,o2) -> Double.compare(o2.getValue(), o1.getValue()))
-            .mapToInt(i-> i.getKey())
-            .toArray();
+
+        // 실패율 내림차순 정렬 후 스테이지 번호 수집
+        List<Integer> answer = new ArrayList<>();
+        for (Map.Entry<Double, List<Integer>> entry : failure.entrySet()) {
+            List<Integer> values = entry.getValue();
+            Collections.sort(values); // 동일 실패율 시 오름차순 정렬
+            for (int v : values) {
+                answer.add(v);
+            }
+        }
+
+        // List -> int[] 변환
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
