83a55c2366ea10cd51153b650ab45e8d3bf8d30e
Jian Kim <gayeon1008@g.skku.edu>
2025-07-21 14:51:54 +0900

    [Gold IV] Title: 거짓말, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -1,65 +1,63 @@
+import static java.lang.System.in;
+
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+    static List<Integer>[] party;
+    static List<Integer>[] willKnowTruth;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken()); // 사람 수
+        int m = Integer.parseInt(st.nextToken()); // 파티 수
 
         st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); // 사람 수
-        int M = Integer.parseInt(st.nextToken()); // 파티 수
-
-        boolean[] knowsTruth = new boolean[N + 1];
-        st = new StringTokenizer(br.readLine());
-        int K = Integer.parseInt(st.nextToken());// 진실을 아는 사람 수
-        List<Integer> truthPeople = new ArrayList<>();
-        for (int i = 0; i < K; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            knowsTruth[num] = true;
-            truthPeople.add(num);
+        int k = Integer.parseInt(st.nextToken()); // 진실을 아는 사람 수
+        List<Integer> knowTruth = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            knowTruth.add(Integer.parseInt(st.nextToken())); // 진실을 아는 사람 번호
         }
 
-        int[] parent = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            parent[i] = i;
+        party = new List[m + 1];
+        for (int i = 0; i < m + 1; i++) {
+            party[i] = new ArrayList<>(); // 초기화
         }
 
-        List[] canAttend = new ArrayList[M + 1];
-        for (int i = 1; i <= M; i++) {
-            canAttend[i] = new ArrayList<>();
+        willKnowTruth = new List[n + 1];
+        for (int i = 0; i <= n; i++) {
+            willKnowTruth[i] = new ArrayList<>(); // 초기화
         }
-        for (int i = 0; i < M; i++) {
+
+        for (int i = 1; i <= m; i++) {
             st = new StringTokenizer(br.readLine());
-            int partySize = Integer.parseInt(st.nextToken());
-            int first = -1;
-            for (int j = 0; j < partySize; j++) {
-                int person = Integer.parseInt(st.nextToken());
-                canAttend[i + 1].add(person);
-                if (j == 0) {
-                    first = person;
-                } else {
-                    union(parent, first, person);
-                }
+            int p = Integer.parseInt(st.nextToken()); // 파티에 참석하는 사람 수
+            for (int j = 0; j < p; j++) {
+                int personNum = Integer.parseInt(st.nextToken());
+                party[i].add(personNum); // 파티에 참석하는 사람 번호
+                willKnowTruth[personNum].add(i); // 해당 사람이 방문하는 파티 번호를 기록
             }
         }
 
-        boolean[] groupHasTruth = new boolean[N + 1];
-        for (int t : truthPeople) {
-            groupHasTruth[find(parent, t)] = true;
+        visited = new boolean[n + 1]; // 진실을 아는 사람 (true), 모르는 사람 (false)
+
+        for (int person : knowTruth) {
+            if (!visited[person]) {
+                dfs(person);
+            }
         }
 
-        int answer = 0;
-        for (int i = 1; i <= M; i++) {
-            List<Integer> list = canAttend[i];
-            boolean canLie = true;
-            for (int person : list) {
-                if (groupHasTruth[find(parent, person)]) {
-                    canLie = false;
+        int answer = 0; // 거짓말을 할 수 있는 파티의 개수
+        for (int i = 1; i <= m; i++) {
+            boolean canLie = true; // 해당 파티에서 거짓말을 할 수 있는지 여부
+            for (int person : party[i]) {
+                if (visited[person]) { // 진실을 아는 사람이 있다면
+                    canLie = false; // 거짓말을 할 수 없음
                     break;
                 }
             }
@@ -67,21 +65,19 @@ public class Main {
                 answer++;
             }
         }
-        System.out.println(answer);
+        System.out.println(answer); // 거짓말을 할 수 있는 파티의 개수 출력
     }
 
-    static int find(int[] parent, int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent, parent[x]);
-        }
-        return parent[x];
-    }
+    static void dfs(int person) {
+        visited[person] = true;
 
-    static void union(int[] parent, int a, int b) {
-        int rootA = find(parent, a);
-        int rootB = find(parent, b);
-        if (rootA != rootB) {
-            parent[rootB] = rootA;
+        for (int nextParty : willKnowTruth[person]) {
+            for (int nextPerson : party[nextParty]) {
+                if (!visited[nextPerson]) {
+                    visited[nextPerson] = true; // 진실을 알게 됨
+                    dfs(nextPerson); // 다음 사람도 진실을 알게 됨
+                }
+            }
         }
     }
 }
