18ad2a42866eaab4532c2d632522bb0b38cb49a9
Jian Kim <gayeon1008@g.skku.edu>
2025-07-25 21:49:14 +0900

    [Gold IV] Title: 키 순서, Time: 728 ms, Memory: 44624 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -1,61 +1,60 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int[][] arr;
+    private static final int INF = 987654321;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		arr = new int[n+1][n+1];
-		
-		while (m --> 0) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			arr[a][b] = 1;
-			arr[b][a] = -1;
-		}
-		
-		for (int k=1; k<=n; k++) {
-			for (int i=1; i<=n; i++) {
-				for (int j=1; j<=n; j++) {
-					if (arr[i][k] == 1 && arr[k][j] == 1) {
-						arr[i][j] = 1;
-					}
-					
-					if (arr[i][k] == -1 && arr[k][j] == -1) {
-						arr[i][j] = -1;
-					}
-				}
-			}
-		}
-		
-		int answer = 0;
-		for (int i=1; i<=n; i++) {
-			int out = 0;
-			int in = 0;
-			for (int j=1; j<=n; j++) {
-				if (arr[i][j] == 1) {
-					in++;
-				} else if (arr[i][j] == -1) {
-					out++;
-				}
-			}
-			
-			if (in + out == n-1) {
-				answer++;
-			}
-		}
-		
-		System.out.println(answer);
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int n = Integer.parseInt(st.nextToken()); // 학생 수
+        int m = Integer.parseInt(st.nextToken()); // 비교 횟수
+
+        int[][] distance = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    distance[i][j] = 0; // 자기 자신과의 거리는 0
+                } else {
+                    distance[i][j] = INF; // 초기값은 무한대로 설정
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()); // 학생 a
+            int b = Integer.parseInt(st.nextToken()); // 학생 b
+
+            distance[a][b] = 1;
+        }
+
+        // 플로이드-워셜 알고리즘
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (distance[i][k] < INF && distance[k][j] < INF) {
+                        distance[i][j] = Math.min(distance[i][j], distance[i][k] + distance[k][j]);
+                    }
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            boolean fixed = true;
+            for (int j = 1; j <= n; j++) {
+                if (distance[i][j] == INF && distance[j][i] == INF) {
+                    fixed = false; // i와 j 사이의 관계가 확정되지 않음
+                }
+            }
+            if (fixed) {
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
+    }
 }
