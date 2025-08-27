372a01f2413076e486b84da394491817443df3bc
Jian Kim <gayeon1008@g.skku.edu>
2025-08-28 00:02:38 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 21.61 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,21 +1,17 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-        Queue<Integer> q1 = new LinkedList<>();
-        Queue<Integer> q2 = new LinkedList<>();
         long sum1 = 0;
         long sum2 = 0;
+        int n = queue1.length;
         
-        for (int num : queue1) {
-            q1.add(num);
-            sum1 += num;
+        int[] mergedQueue = new int[n * 2];
+        for (int i = 0; i < n; i++) {
+            mergedQueue[i] = queue1[i];
+            sum1 += queue1[i];
         }
-        
-        for (int num : queue2) {
-            q2.add(num);
-            sum2 += num;
+        for (int i = 0; i < n; i++) {
+            mergedQueue[i + n] = queue2[i];
+            sum2 += queue2[i];
         }
         
         long totalSum = sum1 + sum2;
@@ -24,35 +20,27 @@ class Solution {
         }
         
         long target = totalSum / 2;
+        
+        int left = 0;
+        int right = n;
+        long currentSum = sum1;
         int count = 0;
         
-        // 큐의 최대 길이 * 3을 반복 횟수 제한으로 설정
-        // 한쪽 큐에 모든 원소를 옮기고 다시 반대쪽으로 옮기는 경우를 고려
-        int limit = (queue1.length + queue2.length) * 2;
-        
-        while (sum1 != target) {
-            if (count > limit) {
-                return -1;
+        while (right < n * 2 && left <= right) {
+            if (currentSum == target) {
+                return count;
             }
             
-            if (sum1 < target) {
-                if (q2.isEmpty()) {
-                    return -1;
-                }
-                int num = q2.poll();
-                q1.add(num);
-                sum1 += num;
-            } else { // sum1 > target
-                if (q1.isEmpty()) {
-                    return -1;
-                }
-                int num = q1.poll();
-                q2.add(num);
-                sum1 -= num;
+            if (currentSum < target) {
+                currentSum += mergedQueue[right];
+                right++;
+            } else { // currentSum > target
+                currentSum -= mergedQueue[left];
+                left++;
             }
             count++;
         }
         
-        return count;
+        return -1;
     }
 }
