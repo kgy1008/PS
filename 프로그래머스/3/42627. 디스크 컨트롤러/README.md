946e375f3c5eeb5a5b74de87199076c4366ca11a
Jian Kim <gayeon1008@g.skku.edu>
2025-09-02 13:54:12 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.66 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
 
 ### 성능 요약
 
-메모리: 79.2 MB, 시간: 2.81 ms
+메모리: 89.7 MB, 시간: 3.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 26일 17:31:34
+2025년 09월 02일 13:54:09
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,36 +2,43 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
-        // 작업을 요청시간 순으로 정렬
-        Arrays.sort(jobs, (a, b) -> Integer.compare(a[0], b[0]));
-        
-        // 소요시간이 짧은 순으로 우선순위 큐 생성
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        
-        int jobIndex = 0;  // 다음에 처리할 작업의 인덱스
-        int currentTime = 0;  // 현재 시간
-        int totalTime = 0;  // 총 대기시간 + 소요시간
-        int completedJobs = 0;  // 완료된 작업 수
-        
-        while (completedJobs < jobs.length) {
-            // 현재 시간까지 요청된 모든 작업을 큐에 추가
-            while (jobIndex < jobs.length && jobs[jobIndex][0] <= currentTime) {
+        // 1. 요청 시점(jobs[i][0]) 기준으로 jobs 배열 정렬
+        Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);
+
+        // 2. 소요 시간(l) 기준으로 오름차순 정렬하는 우선순위 큐
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+
+        int answer = 0;
+        int time = 0;
+        int jobIndex = 0;
+        int count = 0;
+
+        // 3. 모든 작업을 처리할 때까지 반복
+        while (count < jobs.length) {
+            // 현재 시간(time) 이전에 요청된 모든 작업을 우선순위 큐에 추가
+            while (jobIndex < jobs.length && jobs[jobIndex][0] <= time) {
                 pq.offer(jobs[jobIndex]);
                 jobIndex++;
             }
-            
-            if (pq.isEmpty()) {
-                // 처리할 작업이 없으면 다음 작업의 요청시간으로 이동
-                currentTime = jobs[jobIndex][0];
-            } else {
-                // 가장 소요시간이 짧은 작업 처리
+
+            // 우선순위 큐에 작업이 있으면 처리
+            if (!pq.isEmpty()) {
                 int[] job = pq.poll();
-                currentTime += job[1];  // 작업 완료 시간
-                totalTime += (currentTime - job[0]);  // 대기시간 + 소요시간
-                completedJobs++;
+                int requestTime = job[0];
+                int duration = job[1];
+
+                time += duration; // 현재 시간 업데이트
+                answer += (time - requestTime); // 총 대기 시간 누적
+                count++;
+            } else {
+                // 우선순위 큐가 비어있고, 아직 처리할 작업이 남은 경우
+                // CPU가 유휴 상태이므로, 다음 작업의 요청 시간으로 현재 시간을 이동
+                if (jobIndex < jobs.length) {
+                    time = jobs[jobIndex][0];
+                }
             }
         }
         
-        return totalTime / jobs.length;
+        return answer / jobs.length;
     }
 }
