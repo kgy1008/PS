4b3864a4d7bd11d55c54a9efa15c6cb1f2136f2b
Jian Kim <gayeon1008@g.skku.edu>
2025-09-13 23:19:03 +0900

    [Gold III] Title: 커넥티드 카 실험, Time: 1084 ms, Memory: 248144 KB -BaekjoonHub

--- 백준/Gold/25395. 커넥티드 카 실험/커넥티드 카 실험.java
+++ 백준/Gold/25395. 커넥티드 카 실험/커넥티드 카 실험.java
@@ -1,12 +1,8 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Deque;
-import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -28,87 +24,45 @@ public class Main {
             fuels[i] = Integer.parseInt(st.nextToken());
         }
 
-        List<Car> cars = new ArrayList<>();
-        int sIndex = -1; // 시작 차량의 정렬된 인덱스를 저장
-
-        for (int i = 1; i <= n; i++) {
-            Car car = new Car(i, positions[i], fuels[i]);
-            cars.add(car);
-            if (i == s) {
-                sIndex = cars.size() - 1;
-            }
-        }
-
-        cars.sort(Comparator.comparingInt(o -> o.position));
-
-        // 정렬 후의 인덱스 업데이트
-        for (int i = 0; i < n; i++) {
-            cars.get(i).index = i;
-            if (cars.get(i).num == s) {
-                sIndex = i;
-            }
-        }
-
-        Deque<Integer> queue = new ArrayDeque<>();
         boolean[] visited = new boolean[n + 1];
-        TreeSet<Integer> result = new TreeSet<>();
+        Queue<Integer> queue = new ArrayDeque<>();
 
-        // 시작 차량의 정렬된 인덱스를 큐에 추가
-        queue.offer(sIndex);
+        // 시작점 처리
+        queue.offer(s);
         visited[s] = true;
-        result.add(s);
-
-        // 투 포인터
-        int left = sIndex;
-        int right = sIndex;
+        
+        int leftPointer = s - 1;
+        int rightPointer = s + 1;
 
         while (!queue.isEmpty()) {
-            int curIdx = queue.poll();
-            Car curCar = cars.get(curIdx);
+            int cur = queue.poll();
 
-            long startRange = (long) curCar.position - curCar.fuel;
-            long endRange = (long) curCar.position + curCar.fuel;
-
-            // 왼쪽 포인터 이동
-            while (left >= 0 && cars.get(left).position >= startRange) {
-                Car nextCar = cars.get(left);
-                if (!visited[nextCar.num]) {
-                    visited[nextCar.num] = true;
-                    queue.offer(left);
-                    result.add(nextCar.num);
+            int minPos = positions[cur] - fuels[cur];
+            int maxPos = positions[cur] + fuels[cur];
+            
+            while (leftPointer >= 1 && positions[leftPointer] >= minPos) {
+                if (!visited[leftPointer]) {
+                    visited[leftPointer] = true;
+                    queue.offer(leftPointer);
                 }
-                left--;
+                leftPointer--;
             }
 
-            // 오른쪽 포인터 이동
-            while (right < n && cars.get(right).position <= endRange) {
-                Car nextCar = cars.get(right);
-                if (!visited[nextCar.num]) {
-                    visited[nextCar.num] = true;
-                    queue.offer(right);
-                    result.add(nextCar.num);
+            while (rightPointer <= n && positions[rightPointer] <= maxPos) {
+                if (!visited[rightPointer]) {
+                    visited[rightPointer] = true;
+                    queue.offer(rightPointer);
                 }
-                right++;
+                rightPointer++;
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int carNum : result) {
-            sb.append(carNum).append(" ");
+        for (int i = 1; i <= n; i++) {
+            if (visited[i]) {
+                sb.append(i).append(" ");
+            }
         }
-        System.out.print(sb.toString().trim());
-    }
-
-    static class Car {
-        int num;
-        int position;
-        int fuel;
-        int index; // 정렬 후의 인덱스
-
-        Car(int num, int position, int fuel) {
-            this.num = num;
-            this.position = position;
-            this.fuel = fuel;
-        }
+        System.out.println(sb.toString().trim());
     }
 }
