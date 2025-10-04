998a808962d47cb450951e18fa9792b9927b0bf7
Jian Kim <gayeon1008@g.skku.edu>
2025-10-05 00:55:46 +0900

    [Silver V] Title: 최대 상승, Time: 328 ms, Memory: 35168 KB -BaekjoonHub

---
+++ 백준/Silver/25644. 최대 상승/최대 상승.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] nums = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int min = nums[0];
+        int answer = 0;
+
+        for (int i = 1; i < n; i++) {
+            int current = nums[i];
+
+            int currentProfit = current - min;
+            answer = Math.max(answer, currentProfit);
+
+            if (current < min) {
+                min = current;
+            }
+        }
+        System.out.println(answer);
+    }
+}
